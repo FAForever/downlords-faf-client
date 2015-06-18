9b2c69a93e03f8e46c5b98c5feb39572649942b7
Michel Jung <michel.jung89@gmail.com>
2015-06-18 18:46:00 +0200

    switching workplace

--- src/main/java/com/faforever/client/chat/UserInfoWindowController.java
+++ src/main/java/com/faforever/client/chat/UserInfoWindowController.java
@@ -2,7 +2,6 @@ package com.faforever.client.chat;
 
 public class UserInfoWindowController {
 
-
   private PlayerInfoBean playerInfoBean;
 
   public void setPlayerInfoBean(PlayerInfoBean playerInfoBean) {
--- src/main/java/com/faforever/client/network/PortCheckServiceImpl.java
+++ src/main/java/com/faforever/client/network/PortCheckServiceImpl.java
@@ -1,9 +1,28 @@
 package com.faforever.client.network;
 
 import com.faforever.client.i18n.I18n;
+import com.faforever.client.legacy.relay.RelayClientMessage;
+import com.faforever.client.legacy.relay.RelayClientMessageSerializer;
+import com.faforever.client.legacy.relay.RelayServerAction;
+import com.faforever.client.legacy.writer.ServerWriter;
+import com.faforever.client.notification.Action;
+import com.faforever.client.notification.NotificationService;
+import com.faforever.client.notification.PersistentNotification;
+import com.faforever.client.notification.Severity;
+import com.faforever.client.preferences.PreferencesService;
 import com.faforever.client.task.PrioritizedTask;
 import com.faforever.client.task.TaskService;
 import com.faforever.client.util.Callback;
+import com.faforever.client.util.ConcurrentUtil;
+import com.offbynull.portmapper.MappedPort;
+import com.offbynull.portmapper.PortMapper;
+import com.offbynull.portmapper.PortMapperEventListener;
+import com.offbynull.portmapper.PortMapperFactory;
+import com.offbynull.portmapper.PortType;
+import com.sun.corba.se.spi.activation.Server;
+import javafx.application.HostServices;
+import javafx.concurrent.Task;
+import org.jetbrains.annotations.NotNull;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -12,23 +31,40 @@ import org.springframework.core.env.Environment;
 import java.io.DataOutputStream;
 import java.io.IOException;
 import java.lang.invoke.MethodHandles;
+import java.lang.reflect.Array;
 import java.net.ConnectException;
 import java.net.DatagramPacket;
 import java.net.DatagramSocket;
 import java.net.Socket;
 import java.net.SocketTimeoutException;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collection;
+import java.util.List;
 import java.util.Timer;
 import java.util.TimerTask;
 
 import static com.faforever.client.task.PrioritizedTask.Priority.LOW;
+import static com.faforever.client.task.TaskGroup.IMMEDIATE;
 import static com.faforever.client.task.TaskGroup.NET_LIGHT;
 
-public class PortCheckServiceImpl implements PortCheckService {
+/**
+ * Opens the game port and connects to the FAF relay server in order the see whether data on the game port is received.
+ */
+public class PortCheckServiceImpl implements PortCheckService, PortMapperEventListener {
 
   private static final Logger logger = LoggerFactory.getLogger(MethodHandles.lookup().lookupClass());
+
+  private static final String PORT_UNREACHABLE_NOTIFICATION_ID = "portUnreachable";
   private static final int REQUEST_DELAY = 1000;
   private static final int TIMEOUT = 5000;
+  // TODO send a random number that the server should send back
   private static final String EXPECTED_ANSWER = "OK";
+
+  /**
+   * Half an hour.
+   */
+  private static final long UPNP_LIFETIME = 1800;
 
   @Autowired
   TaskService taskService;
@@ -37,35 +73,143 @@ public class PortCheckServiceImpl implements PortCheckService {
   Environment environment;
 
   @Autowired
+  NotificationService notificationService;
+
+  @Autowired
+  PreferencesService preferencesService;
+
+  @Autowired
+  HostServices hostServices;
+
+  @Autowired
   I18n i18n;
 
+  private Collection<GamePortCheckListener> gamePortCheckListeners;
+
+  public PortCheckServiceImpl() {
+    gamePortCheckListeners = new ArrayList<>();
+  }
+
   @Override
-  public void checkUdpPortInBackground(int port, Callback<Boolean> callback) {
-    taskService.submitTask(NET_LIGHT, new PrioritizedTask<Boolean>(i18n.get("portCheckTask.title"), LOW) {
+  public void checkGamePortInBackground() {
+    gamePortCheckListeners.forEach(GamePortCheckListener::onGamePortCheckStarted);
+
+    int port = preferencesService.getPreferences().getForgedAlliance().getPort();
+
+    taskService.submitTask(IMMEDIATE, new PrioritizedTask<Boolean>(i18n.get("portCheckTask.title"), LOW) {
       @Override
       protected Boolean call() throws Exception {
-        String remoteHost = environment.getProperty("portCheck.host");
-        Integer remotePort = environment.getProperty("portCheck.port", Integer.class);
-
-        logger.info("Checking reachability of UDP port {} using port checker service at {}:{}", port, remoteHost, remotePort);
-
-        sendDelayedPackageRequest(remoteHost, remotePort, port);
-
-        try (DatagramSocket datagramSocket = new DatagramSocket(port)) {
-          datagramSocket.setSoTimeout(TIMEOUT);
-
-          byte[] buffer = new byte[EXPECTED_ANSWER.length()];
-          datagramSocket.receive(new DatagramPacket(buffer, buffer.length));
-
-          logger.info("UDP port {} is reachable", port);
-
-          return true;
-        } catch (SocketTimeoutException e) {
-          logger.info("UDP port {} is unreachable", port);
-          return false;
+        Boolean successful = checkPort(port);
+        if (!successful) {
+          logger.info("Port check failed, trying UPnP");
+          mapUpnpPort(port);
+          successful = checkPort(port);
+        }
+        return successful;
+      }
+    }, new Callback<Boolean>() {
+      @Override
+      public void success(Boolean result) {
+        if (!result) {
+          notifyPortUnreachable(port);
+        }
+        for (GamePortCheckListener gamePortCheckListener : gamePortCheckListeners) {
+          gamePortCheckListener.onGamePortCheckResult(result);
         }
       }
-    }, callback);
+
+      @Override
+      public void error(Throwable e) {
+        logger.info("Port check failed", e);
+      }
+    });
+  }
+
+  private void mapUpnpPort(int port) throws IOException, InterruptedException {
+    try (PortMapper portMapper = PortMapperFactory.create(this)) {
+      MappedPort mappedPort = portMapper.mapPort(PortType.TCP, port, UPNP_LIFETIME);
+
+      logger.info("Created port mapping from {}:{} to port {}",
+          mappedPort.getExternalAddress(),
+          mappedPort.getExternalPort(),
+          mappedPort.getInternalPort());
+
+      ConcurrentUtil.executeInBackground(new Task<Void>() {
+        @Override
+        protected Void call() throws Exception {
+          try {
+            while (!isCancelled()) {
+              MappedPort refreshedPortMapping = portMapper.refreshPort(mappedPort, mappedPort.getLifetime());
+
+              logger.debug("Port mapping refreshed from {}:{} to port {}",
+                  refreshedPortMapping.getExternalAddress(),
+                  refreshedPortMapping.getExternalPort(),
+                  refreshedPortMapping.getInternalPort());
+
+              Thread.sleep(mappedPort.getLifetime() / 2 * 1000);
+            }
+          } finally {
+            portMapper.unmapPort(mappedPort);
+          }
+
+          return null;
+        }
+      });
+    }
+  }
+
+  @Override
+  public void addGamePortCheckListener(GamePortCheckListener listener) {
+    gamePortCheckListeners.add(listener);
+  }
+
+  @NotNull
+  private Boolean checkPort(int port) throws IOException {
+    String remoteHost = environment.getProperty("relay.host");
+    Integer remotePort = environment.getProperty("relay.port", Integer.class);
+
+    logger.info("Checking reachability of UDP port {} using port checker service at {}:{}", port, remoteHost, remotePort);
+
+    sendDelayedPackageRequest(remoteHost, remotePort, port);
+
+    try (DatagramSocket datagramSocket = new DatagramSocket(port)) {
+      datagramSocket.setSoTimeout(TIMEOUT);
+
+      byte[] buffer = new byte[EXPECTED_ANSWER.length()];
+      datagramSocket.receive(new DatagramPacket(buffer, buffer.length));
+
+      logger.info("UDP port {} is reachable", port);
+
+      return true;
+    } catch (SocketTimeoutException e) {
+      logger.info("UDP port {} is unreachable", port);
+      return false;
+    }
+  }
+
+  /**
+   * Notifies the user about port unreachability.
+   */
+
+  private void notifyPortUnreachable(int port) {
+    List<Action> actions = Arrays.asList(
+        new Action(
+            i18n.get("portCheckTask.help"),
+            event -> hostServices.showDocument(environment.getProperty("portCheck.helpUrl"))
+        ),
+        new Action(
+            i18n.get("portCheckTask.neverShowAgain"),
+            event -> preferencesService.getPreferences().getIgnoredNotifications().add(PORT_UNREACHABLE_NOTIFICATION_ID)
+        ),
+        new Action(
+            i18n.get("portCheckTask.retry"),
+            event -> checkGamePortInBackground()
+        )
+    );
+
+    notificationService.addNotification(
+        new PersistentNotification(i18n.get("portCheckTask.unreachableNotification", port), Severity.WARN, actions)
+    );
   }
 
   /**
@@ -78,15 +222,27 @@ public class PortCheckServiceImpl implements PortCheckService {
       public void run() {
         try {
           try (Socket socket = new Socket(remoteHost, remotePort);
-               DataOutputStream dataOutputStream = new DataOutputStream(socket.getOutputStream())) {
-            dataOutputStream.writeInt(port);
+               ServerWriter serverWriter = createServerWriter(socket)) {
+            serverWriter.write(new RelayClientMessage(RelayServerAction.GAME_STATE, Arrays.asList("idle")));
           }
         } catch (ConnectException e) {
           logger.warn("Port check server is not reachable");
-        } catch(IOException e) {
+        } catch (IOException e) {
           throw new RuntimeException(e);
         }
       }
-    }, 1000);
+    }, REQUEST_DELAY);
+  }
+
+  @Override
+  public void resetRequired(String details) {
+
+  }
+
+
+  private ServerWriter createServerWriter(Socket fafSocket) throws IOException {
+    ServerWriter serverWriter = new ServerWriter(fafSocket.getOutputStream());
+    serverWriter.registerObjectWriter(new RelayClientMessageSerializer(), RelayClientMessage.class);
+    return serverWriter;
   }
 }
