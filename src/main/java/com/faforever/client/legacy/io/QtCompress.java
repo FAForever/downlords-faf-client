5207a2bd3b0ba0050035b7fb661dce5bb4581f41
Michel Jung <michel.jung89@gmail.com>
2015-07-30 01:23:52 +0200

    Unit tests for QtCompress

--- src/main/java/com/faforever/client/legacy/io/QtCompress.java
+++ src/main/java/com/faforever/client/legacy/io/QtCompress.java
@@ -9,12 +9,19 @@ import java.util.zip.DeflaterOutputStream;
 import java.util.zip.Inflater;
 import java.util.zip.InflaterOutputStream;
 
+/**
+ * Utility class that compresses and uncompresses bytes like QT's <a href="http://doc.qt.io/qt-5/qbytearray.html">QByteArray</a>.
+ */
 public final class QtCompress {
 
   private QtCompress() {
     throw new AssertionError("Not instantiatable");
   }
 
+  /**
+   * Compresses the specified bytes like <a href="http://doc.qt.io/qt-5/qbytearray.html#qCompress">QByteArray.qCompress()</a>
+   * does.
+   */
   public static byte[] qUncompress(byte[] bytes) throws IOException {
     Inflater inflater = new Inflater();
     inflater.setInput(Arrays.copyOfRange(bytes, 4, bytes.length));
@@ -28,6 +35,10 @@ public final class QtCompress {
     return byteArray.toByteArray();
   }
 
+  /**
+   * Uncompresses the specified bytes like <a href="http://doc.qt.io/qt-5/qbytearray.html#qCompress">QByteArray.qCompress()</a>
+   * does.
+   */
   public static byte[] qCompress(byte[] bytes) throws IOException {
     Deflater deflater = new Deflater();
     deflater.setInput(bytes);
---
+++ src/test/java/com/faforever/client/legacy/io/QtCompressTest.java
@@ -0,0 +1,37 @@
+package com.faforever.client.legacy.io;
+
+import org.junit.Test;
+
+import java.nio.charset.StandardCharsets;
+
+import static com.faforever.client.test.IsUtilityClassMatcher.isUtilityClass;
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.junit.Assert.assertArrayEquals;
+
+public class QtCompressTest {
+
+  public static final byte[] UNCOMPRESSED_BYTES = "AnyBytes".getBytes(StandardCharsets.US_ASCII);
+  public static final byte[] COMPRESSED_BYTES = new byte[]{
+      // Number of uncompressed bytes
+      0, 0, 0, (byte) UNCOMPRESSED_BYTES.length,
+      // Compressed bytes
+      120, -100, 114, -52, -85, 116, -86, 44, 73, 45, 6, 0, 0, 0, -1, -1, 3, 0, 13, -81, 3, 48
+  };
+
+  @Test
+  public void testIsUtilityClass() {
+    assertThat(QtCompress.class, isUtilityClass());
+  }
+
+  @Test
+  public void testQCompress() throws Exception {
+    byte[] compressedBytes = QtCompress.qCompress(UNCOMPRESSED_BYTES);
+    assertArrayEquals(COMPRESSED_BYTES, compressedBytes);
+  }
+
+  @Test
+  public void testQUncompress() throws Exception {
+    byte[] uncompressedBytes = QtCompress.qUncompress(COMPRESSED_BYTES);
+    assertArrayEquals(UNCOMPRESSED_BYTES, uncompressedBytes);
+  }
+}
