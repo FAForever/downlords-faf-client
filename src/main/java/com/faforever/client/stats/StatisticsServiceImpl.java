1c8b4a66d68a6164736a6598cacd9f2618927088
Michel Jung <michel.jung89@gmail.com>
2016-06-20 23:55:38 +0200

    WIP #241

--- src/main/java/com/faforever/client/stats/StatisticsServiceImpl.java
+++ src/main/java/com/faforever/client/stats/StatisticsServiceImpl.java
@@ -1,9 +1,7 @@
 package com.faforever.client.stats;
 
-import com.faforever.client.config.CacheNames;
 import com.faforever.client.legacy.StatisticsServerAccessor;
 import com.faforever.client.remote.domain.StatisticsType;
-import org.springframework.cache.annotation.Cacheable;
 
 import javax.annotation.Resource;
 import java.util.concurrent.CompletableFuture;
@@ -14,7 +12,6 @@ public class StatisticsServiceImpl implements StatisticsService {
   StatisticsServerAccessor statisticsServerAccessor;
 
   @Override
-  @Cacheable(value = CacheNames.STATISTICS, key = "#type + #username")
   public CompletableFuture<PlayerStatisticsMessage> getStatisticsForPlayer(StatisticsType type, String username) {
     return statisticsServerAccessor.requestPlayerStatistics(type, username);
   }
