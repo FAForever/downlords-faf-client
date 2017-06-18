78ca32552a063f1ba2d2a9c6a47b947543bae2a6
Michel Jung <michel.jung89@gmail.com>
2017-06-18 23:17:36 +0200

    Fix coop leaderboard not loading

--- src/main/java/com/faforever/client/api/FafApiAccessorImpl.java
+++ src/main/java/com/faforever/client/api/FafApiAccessorImpl.java
@@ -410,7 +410,7 @@ public class FafApiAccessorImpl implements FafApiAccessor {
   @Override
   @Cacheable(CacheNames.COOP_LEADERBOARD)
   public List<CoopResult> getCoopLeaderboard(String missionId, int numberOfPlayers) {
-    return getMany("/data/coopLeaderboard", numberOfPlayers, ImmutableMap.of(
+    return getMany("/data/coopResult", numberOfPlayers, ImmutableMap.of(
         "filter", rsql(qBuilder().intNum("playerCount").eq(numberOfPlayers)),
         "sort", "-duration"
     ));
