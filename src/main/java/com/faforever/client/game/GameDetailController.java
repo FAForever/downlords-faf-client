a46f0cc4f1381cb9ade362483bdd0d062585030f
axel1200 <alexander.von.trostorff@gmail.com>
2017-09-28 23:19:52 +0200

    Refactored code Main thing I did was to give game, that is set the Controller the name newGame, so you do not get confused with game and this.game. Further I removed the Weakreference of the Invalidationlistener. Weakreferences are meant to be used if the listeners might not the removed correctly to avoid memory leakages. So this is not needed in this context and makes removing the listener more difficult. On Top a have a second check if the correct game is updated. Fixes #573

--- src/main/java/com/faforever/client/game/GameDetailController.java
+++ src/main/java/com/faforever/client/game/GameDetailController.java
@@ -10,7 +10,6 @@ import com.faforever.client.player.PlayerService;
 import com.faforever.client.theme.UiService;
 import com.faforever.client.util.ProgrammingError;
 import javafx.beans.InvalidationListener;
-import javafx.beans.WeakInvalidationListener;
 import javafx.collections.ObservableMap;
 import javafx.event.ActionEvent;
 import javafx.scene.Node;
@@ -19,10 +18,13 @@ import javafx.scene.image.ImageView;
 import javafx.scene.layout.Pane;
 import javafx.scene.layout.VBox;
 import org.apache.commons.lang3.StringUtils;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.config.ConfigurableBeanFactory;
 import org.springframework.context.annotation.Scope;
 import org.springframework.stereotype.Component;
 
+import java.lang.invoke.MethodHandles;
 import java.util.List;
 
 import static javafx.beans.binding.Bindings.createObjectBinding;
@@ -32,6 +34,7 @@ import static javafx.beans.binding.Bindings.createStringBinding;
 @Scope(ConfigurableBeanFactory.SCOPE_PROTOTYPE)
 public class GameDetailController implements Controller<Pane> {
 
+  private static final Logger logger = LoggerFactory.getLogger(MethodHandles.lookup().lookupClass());
   private final I18n i18n;
   private final MapService mapService;
   private final ModService modService;
@@ -91,43 +94,48 @@ public class GameDetailController implements Controller<Pane> {
     }
   }
 
-  public void setGame(Game game) {
-    this.game = game;
-    if (game == null) {
+  public void setGame(Game newGame) {
+    if (newGame == null) {
       joinButton.setVisible(false);
       watchButton.setVisible(false);
       return;
     }
-    gameTitleLabel.textProperty().bind(game.titleProperty());
-    hostLabel.textProperty().bind(game.hostProperty());
-    mapLabel.textProperty().bind(game.mapFolderNameProperty());
+    gameTitleLabel.textProperty().bind(newGame.titleProperty());
+    hostLabel.textProperty().bind(newGame.hostProperty());
+    mapLabel.textProperty().bind(newGame.mapFolderNameProperty());
     numberOfPlayersLabel.textProperty().bind(createStringBinding(
-        () -> i18n.get("game.detail.players.format", game.getNumPlayers(), game.getMaxPlayers()),
-        game.numPlayersProperty(),
-        game.maxPlayersProperty()
+        () -> i18n.get("game.detail.players.format", newGame.getNumPlayers(), newGame.getMaxPlayers()),
+        newGame.numPlayersProperty(),
+        newGame.maxPlayersProperty()
     ));
     mapImageView.imageProperty().bind(createObjectBinding(
-        () -> mapService.loadPreview(game.getMapFolderName(), PreviewSize.LARGE),
-        game.mapFolderNameProperty()
+        () -> mapService.loadPreview(newGame.getMapFolderName(), PreviewSize.LARGE),
+        newGame.mapFolderNameProperty()
     ));
     gameTypeLabel.textProperty().bind(createStringBinding(() -> {
-      FeaturedMod gameType = modService.getFeaturedMod(game.getFeaturedMod()).get();
+      FeaturedMod gameType = modService.getFeaturedMod(newGame.getFeaturedMod()).get();
       String fullName = gameType != null ? gameType.getDisplayName() : null;
       return StringUtils.defaultString(fullName);
-    }, game.featuredModProperty()));
+    }, newGame.featuredModProperty()));
 
     if (this.game != null && teamsInvalidationListener != null) {
       this.game.getTeams().removeListener(teamsInvalidationListener);
     }
 
-    teamsInvalidationListener = observable -> createTeams(game.getTeams());
-    teamsInvalidationListener.invalidated(game.getTeams());
-    game.getTeams().addListener(new WeakInvalidationListener(teamsInvalidationListener));
-    game.statusProperty().addListener(gameStatusInvalidationListener);
-    gameStatusInvalidationListener.invalidated(game.statusProperty());
+    this.game = newGame;
+
+    teamsInvalidationListener = observable -> createTeams(newGame.getTeams(), newGame);
+    teamsInvalidationListener.invalidated(newGame.getTeams());
+    newGame.getTeams().addListener(teamsInvalidationListener);
+    newGame.statusProperty().addListener(gameStatusInvalidationListener);
+    gameStatusInvalidationListener.invalidated(newGame.statusProperty());
   }
 
-  private void createTeams(ObservableMap<? extends String, ? extends List<String>> playersByTeamNumber) {
+  private void createTeams(ObservableMap<? extends String, ? extends List<String>> playersByTeamNumber, Game game) {
+    if (!game.equals(this.game)) {
+      logger.warn("Wrong game updated");
+      return;
+    }
     teamListPane.getChildren().clear();
     synchronized (playersByTeamNumber) {
       TeamCardController.createAndAdd(playersByTeamNumber, playerService, uiService, teamListPane);
