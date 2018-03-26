a82d6bcd85771b512437e77eab5f3bff5ce1557b
Michel Jung <michel.jung89@gmail.com>
2018-03-26 16:32:31 +0200

    #535 Add Czech and French

--- src/main/java/com/faforever/client/preferences/LanguageInfo.java
+++ src/main/java/com/faforever/client/preferences/LanguageInfo.java
@@ -3,9 +3,11 @@ package com.faforever.client.preferences;
 
 public enum LanguageInfo {
   AUTO(null, null, "settings.languages.system"),
-  EN("en", "UK", "settings.languages.english"),
-  DE("de", "DE", "settings.languages.german"),
-  RU("ru", "RU", "settings.languages.russian");
+  EN("en", "", "settings.languages.english"),
+  DE("de", "", "settings.languages.german"),
+  CS("cs", "", "settings.languages.czech"),
+  FR("fr", "", "settings.languages.french"),
+  RU("ru", "", "settings.languages.russian");
 
   private final String languageCode;
   private final String countryCode;
