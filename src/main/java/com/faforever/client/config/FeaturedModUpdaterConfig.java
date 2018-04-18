4e7ca3c94f62e8a8eb5b22e1231ae023336c6d7d
Alexander von Trostorff <alexander.von.trostorff@gmail.com>
2018-04-18 20:02:21 +0200

    Remove git updater
    Fixes #817

--- src/main/java/com/faforever/client/config/FeaturedModUpdaterConfig.java
+++ src/main/java/com/faforever/client/config/FeaturedModUpdaterConfig.java
@@ -3,11 +3,9 @@ package com.faforever.client.config;
 import com.faforever.client.FafClientApplication;
 import com.faforever.client.game.FaInitGenerator;
 import com.faforever.client.mod.ModService;
-import com.faforever.client.notification.NotificationService;
 import com.faforever.client.patch.BireusFeaturedModUpdater;
 import com.faforever.client.patch.GameUpdater;
 import com.faforever.client.patch.GameUpdaterImpl;
-import com.faforever.client.patch.GitRepositoryFeaturedModUpdater;
 import com.faforever.client.patch.SimpleHttpFeaturedModUpdater;
 import com.faforever.client.remote.FafService;
 import com.faforever.client.task.TaskService;
@@ -27,16 +25,13 @@ public class FeaturedModUpdaterConfig {
   private final TaskService taskService;
   private final FafService fafService;
   private final FaInitGenerator faInitGenerator;
-  private final GitRepositoryFeaturedModUpdater gitFeaturedModUpdater;
   private final SimpleHttpFeaturedModUpdater httpFeaturedModUpdater;
   private final BireusFeaturedModUpdater bireusFeaturedModUpdater;
-  private final NotificationService notificationService;
 
   @Bean
   GameUpdater gameUpdater() {
     return new GameUpdaterImpl(modService, applicationContext, taskService, fafService, faInitGenerator)
         .addFeaturedModUpdater(bireusFeaturedModUpdater)
-        .addFeaturedModUpdater(gitFeaturedModUpdater)
         .addFeaturedModUpdater(httpFeaturedModUpdater);
   }
 }
