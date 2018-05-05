e0a8547d27cdfd4b664213fd2d846b2cdc1d0dee
Dylan Kumar <dylan.kumar@icloud.com>
2018-05-05 20:43:49 +1200

    removing interfaces

--- src/main/java/com/faforever/client/clan/ClanService.java
+++
@@ -1,9 +0,0 @@
-package com.faforever.client.clan;
-
-import java.util.Optional;
-import java.util.concurrent.CompletableFuture;
-
-public interface ClanService {
-
-  CompletableFuture<Optional<Clan>> getClanByTag(String tag);
-}
