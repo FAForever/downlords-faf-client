71d5f3b78a0498af6de3cdf19f65f47dba912dd6
geosearchef <geosearchef@googlemail.com>
2018-05-24 14:54:51 +0200

    set posix uid executable permission
    closes #796

--- src/main/java/com/faforever/client/legacy/PosixUidService.java
+++ src/main/java/com/faforever/client/legacy/PosixUidService.java
@@ -1,22 +1,28 @@
 package com.faforever.client.legacy;
 
 import com.faforever.client.os.OsUtils;
+import com.google.common.collect.Sets;
 import org.springframework.context.annotation.Lazy;
 import org.springframework.context.annotation.Profile;
 import org.springframework.stereotype.Service;
 
 import java.io.IOException;
+import java.nio.file.Files;
 import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.nio.file.attribute.PosixFilePermission;
 
 
 @Lazy
 @Service
 @Profile({"linux", "mac"})
-public class UnixUidService implements UidService {
+public class PosixUidService implements UidService {
 
   @Override
   public String generate(String sessionId, Path logFile) throws IOException {
     String uidDir = System.getProperty("nativeDir", "lib");
-    return OsUtils.execAndGetOutput(String.format("%s/faf-uid %s", uidDir, sessionId));
+    Path uidPath = Paths.get(uidDir).resolve("faf-uid");
+    Files.setPosixFilePermissions(uidPath, Sets.immutableEnumSet(PosixFilePermission.OWNER_READ, PosixFilePermission.OWNER_EXECUTE));
+    return OsUtils.execAndGetOutput(String.format("%s %s", uidPath.toAbsolutePath(), sessionId));
   }
 }
