ad5655c4334b2e41cacb335f5b147e5a8a5aef47
Super4121 <40344333+Super4121@users.noreply.github.com>
2018-07-12 07:35:30 +0600

    4c

--- src/main/java/com/faforever/client/fa/LaunchCommandBuilder.java
+++ src/main/java/com/faforever/client/fa/LaunchCommandBuilder.java
@@ -6,7 +6,6 @@ import org.springframework.util.StringUtils;
 
 import java.net.Inet4Address;
 import java.net.URI;
-import java.net.URISyntaxException;
 import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.util.ArrayList;
