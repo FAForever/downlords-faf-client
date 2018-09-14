efadb5c4ebe8bb9e498a5371cd0598c0239318cc
Geosearchef <geosearchef@googlemail.com>
2018-09-14 20:11:36 +0200

    update ice adapter version, build script for archive

--- src/main/java/com/faforever/client/remote/domain/IceServerMessage.java
+++ src/main/java/com/faforever/client/remote/domain/IceServerMessage.java
@@ -3,8 +3,6 @@ package com.faforever.client.remote.domain;
 import com.faforever.client.fa.relay.GpgServerMessage;
 import com.faforever.client.fa.relay.GpgServerMessageType;
 
-import java.util.Map;
-
 public class IceServerMessage extends GpgServerMessage {
   private static final int SENDER_INDEX = 0;
   private static final int RECORD_INDEX = 1;
@@ -17,7 +15,7 @@ public class IceServerMessage extends GpgServerMessage {
     return getInt(SENDER_INDEX);
   }
 
-  public Map<String, Object> getRecord() {
+  public Object getRecord() {
     return getObject(RECORD_INDEX);
   }
 }
