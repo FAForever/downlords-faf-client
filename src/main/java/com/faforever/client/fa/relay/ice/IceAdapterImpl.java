e3791cea35c2a64375b8a2370594bc62cdece9be
Brutus5000 <Brutus5000@gmx.net>
2019-04-22 21:59:01 +0200

    Adapt to ICE Adapter 2.3.0

--- src/main/java/com/faforever/client/fa/relay/ice/IceAdapterImpl.java
+++ src/main/java/com/faforever/client/fa/relay/ice/IceAdapterImpl.java
@@ -182,6 +182,11 @@ public class IceAdapterImpl implements IceAdapter, InitializingBean, DisposableB
 
       if (clientProperties.isShowIceAdapterDebugWindow()) {
         cmd.add("--debug-window");
+        cmd.add("--info-window");
+      } else {
+        cmd.add("--info-window");
+        cmd.add("--delay-ui");
+        cmd.add("10000");
       }
 
       try {
