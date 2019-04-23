15745bd3f339c14dddcecd8f6600af2942213a29
Martin Müllenhaupt <mm+git@netlair.de>
2019-04-23 13:28:18 +0200

    set faf-uid file permission after download (might fix #1162)

--- src/main/java/com/faforever/client/legacy/PosixUidService.java
+++ src/main/java/com/faforever/client/legacy/PosixUidService.java
@@ -7,7 +7,6 @@ import org.springframework.context.annotation.Profile;
 import org.springframework.stereotype.Service;
 
 import java.io.IOException;
-import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.nio.file.attribute.PosixFilePermission;
@@ -22,7 +21,6 @@ public class PosixUidService implements UidService {
   public String generate(String sessionId, Path logFile) throws IOException {
     String uidDir = System.getProperty("nativeDir", "lib");
     Path uidPath = Paths.get(uidDir).resolve("faf-uid");
-    Files.setPosixFilePermissions(uidPath, Sets.immutableEnumSet(PosixFilePermission.OWNER_READ, PosixFilePermission.OWNER_EXECUTE));
     return OsUtils.execAndGetOutput(String.format("%s %s", uidPath.toAbsolutePath(), sessionId));
   }
 }
