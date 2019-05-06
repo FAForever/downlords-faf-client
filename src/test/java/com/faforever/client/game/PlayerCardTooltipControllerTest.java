5b3c1c4117a83cbb7efa8b3495d524d270fb9ced
Alexander von Trostorff <19996713+axel1200@users.noreply.github.com>
2019-05-06 21:44:36 +0200

    Show angry smile after foe name (#892)
    and group icon if player is in your friend group
    Fixes #887

---
+++ src/test/java/com/faforever/client/game/PlayerCardTooltipControllerTest.java
@@ -0,0 +1,75 @@
+package com.faforever.client.game;
+
+import com.faforever.client.chat.CountryFlagService;
+import com.faforever.client.i18n.I18n;
+import com.faforever.client.player.Player;
+import com.faforever.client.player.PlayerBuilder;
+import com.faforever.client.player.SocialStatus;
+import com.faforever.client.test.AbstractPlainJavaFxTest;
+import org.junit.Before;
+import org.junit.Test;
+import org.mockito.Mock;
+
+import static org.hamcrest.core.Is.is;
+import static org.junit.Assert.assertThat;
+import static org.mockito.Mockito.when;
+
+public class PlayerCardTooltipControllerTest extends AbstractPlainJavaFxTest {
+  @Mock
+  private I18n i18n;
+  @Mock
+  private CountryFlagService countryFlagService;
+
+  private PlayerCardTooltipController instance;
+
+  @Before
+  public void setUp() throws Exception {
+    instance = new PlayerCardTooltipController(countryFlagService, i18n);
+    loadFxml("theme/player_card_tooltip.fxml", clazz -> instance);
+  }
+
+  @Test
+  public void testSetFoe() {
+    when(i18n.get("userInfo.tooltipFormat", "foe", 1000)).thenReturn("foe(1000)");
+
+    PlayerBuilder playerBuilder = PlayerBuilder.create("foe")
+        .defaultValues()
+        .socialStatus(SocialStatus.FOE);
+    Player player = playerBuilder.get();
+    instance.setPlayer(player, 1000);
+
+    assertThat(instance.foeIconText.isVisible(), is(true));
+    assertThat(instance.friendIconText.isVisible(), is(false));
+    assertThat(instance.playerInfo.getText(), is("foe(1000)"));
+  }
+
+  @Test
+  public void testSetFriend() {
+    when(i18n.get("userInfo.tooltipFormat", "user", 1000)).thenReturn("user(1000)");
+
+    PlayerBuilder playerBuilder = PlayerBuilder.create("user")
+        .defaultValues()
+        .socialStatus(SocialStatus.FRIEND);
+    Player player = playerBuilder.get();
+    instance.setPlayer(player, 1000);
+
+    assertThat(instance.foeIconText.isVisible(), is(false));
+    assertThat(instance.friendIconText.isVisible(), is(true));
+    assertThat(instance.playerInfo.getText(), is("user(1000)"));
+  }
+
+  @Test
+  public void testSetOther() {
+    when(i18n.get("userInfo.tooltipFormat", "user", 1000)).thenReturn("user(1000)");
+
+    PlayerBuilder playerBuilder = PlayerBuilder.create("user")
+        .defaultValues()
+        .socialStatus(SocialStatus.OTHER);
+    Player player = playerBuilder.get();
+    instance.setPlayer(player, 1000);
+
+    assertThat(instance.foeIconText.isVisible(), is(false));
+    assertThat(instance.friendIconText.isVisible(), is(false));
+    assertThat(instance.playerInfo.getText(), is("user(1000)"));
+  }
+}
