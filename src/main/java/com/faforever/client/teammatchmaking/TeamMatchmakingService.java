e7d29568cbd60339c4c1277362b9ceaf067d01c9
Geosearchef <geosearchef@googlemail.com>
2019-06-26 00:09:46 +0200

    team matchmaking WIP

--- src/main/java/com/faforever/client/teammatchmaking/TeamMatchmakingService.java
+++ src/main/java/com/faforever/client/teammatchmaking/TeamMatchmakingService.java
@@ -1,16 +1,26 @@
 package com.faforever.client.teammatchmaking;
 
+import com.faforever.client.i18n.I18n;
+import com.faforever.client.notification.Action;
+import com.faforever.client.notification.Action.ActionCallback;
+import com.faforever.client.notification.NotificationService;
+import com.faforever.client.notification.PersistentNotification;
+import com.faforever.client.notification.Severity;
+import com.faforever.client.notification.TransientNotification;
 import com.faforever.client.player.Player;
 import com.faforever.client.player.PlayerService;
 import com.faforever.client.remote.FafServerAccessor;
 import com.faforever.client.remote.domain.PartyInfoMessage;
 import com.faforever.client.remote.domain.PartyInviteMessage;
 import com.faforever.client.remote.domain.PartyKickedMessage;
+import com.faforever.client.util.IdenticonUtil;
 import lombok.Getter;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.InitializingBean;
 import org.springframework.context.annotation.Lazy;
 import org.springframework.stereotype.Service;
+
+import java.util.Collections;
 
 @Lazy
 @Service
@@ -19,14 +29,18 @@ public class TeamMatchmakingService implements InitializingBean {
 
   private final FafServerAccessor fafServerAccessor;
   private final PlayerService playerService;
+  private final NotificationService notificationService;
+  private final I18n i18n;
 
   @Getter
   private Party party;
 
 
-  public TeamMatchmakingService(FafServerAccessor fafServerAccessor, PlayerService playerService) {
+  public TeamMatchmakingService(FafServerAccessor fafServerAccessor, PlayerService playerService, NotificationService notificationService, I18n i18n) {
     this.fafServerAccessor = fafServerAccessor;
     this.playerService = playerService;
+    this.notificationService = notificationService;
+    this.i18n = i18n;
 
     fafServerAccessor.addOnMessageListener(PartyInviteMessage.class, this::onPartyInvite);
     fafServerAccessor.addOnMessageListener(PartyKickedMessage.class, this::onPartyKicked);
@@ -48,7 +62,31 @@ public class TeamMatchmakingService implements InitializingBean {
   }
 
   public void onPartyInvite(PartyInviteMessage message) {
-    //TODO:
+    playerService.getPlayersByIds(Collections.singletonList(message.getSender()))
+        .thenAccept(players -> {
+          if (players.isEmpty()) {
+            log.error("User {} of party invite not found", message.getSender());
+            return;
+          }
+          Player player = players.get(0);
+          ActionCallback callback = event -> fafServerAccessor.acceptPartyInvite(player);
+
+          notificationService.addNotification(new TransientNotification(
+              i18n.get("teammatchmaking.notification.invite.title"),
+              i18n.get("teammatchmaking.notification.invite.message", player.getUsername()),
+              IdenticonUtil.createIdenticon(player.getId()),
+              callback
+          ));
+
+          notificationService.addNotification(new PersistentNotification(
+              i18n.get("teammatchmaking.notification.invite.message", player.getUsername()),
+              Severity.INFO,
+              Collections.singletonList(new Action(
+                  i18n.get("teammatchmaking.notification.invite.join"),
+                  callback
+              ))
+          ));
+        });
   }
 
   public void onPartyKicked(PartyKickedMessage message) {
