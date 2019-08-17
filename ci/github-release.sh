a3c819f3cfaa990e0bb6f5fddff238b9096488e9
Jan <katharsas@users.noreply.github.com>
2019-08-17 23:58:13 +0200

    revert github-release.sh

--- ci/github-release.sh
+++ ci/github-release.sh
@@ -1,15 +1,12 @@
 #!/usr/bin/env bash
 
 # Download install4j and the required JRE, only if it doesn't already exist (from Travis cache)
-INSTALL4J_DIR="$HOME/install4j/install4j8.0"
-JRE_PLACE="$HOME/.install4j8"
-if [[ ! -d "${INSTALL4J_DIR}" || ! -d "${JRE_PLACE}" ]]; then
-  rm -rf "$HOME/install4j"
-  rm -rf "$JRE_PLACE"
-  mkdir "$HOME/install4j"
-  curl https://download-keycdn.ej-technologies.com/install4j/install4j_unix_8_0.tar.gz -o "$HOME/install4j/install4j.tar.gz"
-  mkdir -p "${JRE_PLACE}/jres/"
-  curl http://content.faforever.com/jre/windows-amd64-10.0.2.tar.gz -o "${JRE_PLACE}/jres/windows-amd64-10.0.2.tar.gz"
+INSTALL4J_DIR="$HOME/install4j/install4j7.0.10"
+if [ ! -d "${INSTALL4J_DIR}" ]; then
+  mkdir -p "$HOME/install4j"
+  curl https://download-keycdn.ej-technologies.com/install4j/install4j_unix_7_0_10.tar.gz -o "$HOME/install4j/install4j.tar.gz"
+  mkdir -p "${INSTALL4J_DIR}/jres/"
+  curl http://content.faforever.com/jre/windows-amd64-10.0.2.tar.gz -o "${INSTALL4J_DIR}/jres/windows-amd64-10.0.2.tar.gz"
   tar xzf "$HOME/install4j/install4j.tar.gz" -C "$HOME/install4j"
 fi
 
