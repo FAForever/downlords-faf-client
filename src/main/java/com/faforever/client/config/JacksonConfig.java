fa8d1a0485d75706ef335276042220e7fff2d7cb
Jan <katharsas@users.noreply.github.com>
2019-08-19 02:36:09 +0200

    Properly encode URLs for all jackson objects

--- src/main/java/com/faforever/client/config/JacksonConfig.java
+++ src/main/java/com/faforever/client/config/JacksonConfig.java
@@ -10,6 +10,9 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 import java.io.IOException;
+import java.net.URI;
+import java.net.URISyntaxException;
+import java.net.URL;
 
 @Configuration
 public class JacksonConfig {
@@ -18,6 +21,7 @@ public class JacksonConfig {
   public Module customDeserializers() {
     SimpleModule fafModule = new SimpleModule();
     fafModule.addDeserializer(ComparableVersion.class, new ComparableVersionDeserializer());
+    fafModule.addDeserializer(URL.class, new UrlDeserializer());
     return fafModule;
   }
 
@@ -26,6 +30,21 @@ public class JacksonConfig {
     @Override
     public ComparableVersion deserialize(JsonParser p, DeserializationContext ctxt) throws IOException {
       return new ComparableVersion(p.getValueAsString());
+    }
+  }
+
+  private static class UrlDeserializer extends JsonDeserializer<URL> {
+
+    @Override
+    public URL deserialize(JsonParser p, DeserializationContext ctxt) throws IOException {
+      // the default URL parsing is bad, see https://stackoverflow.com/questions/10786042/java-url-encoding-of-query-string-parameters
+      URL url = new URL(p.getValueAsString());
+      try {
+        URI uri = new URI(url.getProtocol(), url.getUserInfo(), url.getHost(), url.getPort(), url.getPath(), url.getQuery(), url.getRef());
+        return uri.toURL();
+      } catch (URISyntaxException e) {
+        throw new IOException("Could not parse URL!", e);
+      }
     }
   }
 }
