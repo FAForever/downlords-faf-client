585d5359ec8e4db0a73aefbf44dbd909ec27f52b
Jan <Katharsas@users.noreply.github.com>
2019-08-28 23:25:36 +0200

    Properly encode URLs for all jackson objects
    Properly encode URLs for all jackson objects
    
    Fixes #1373

--- src/main/java/com/faforever/client/config/JacksonConfig.java
+++ src/main/java/com/faforever/client/config/JacksonConfig.java
@@ -5,11 +5,16 @@ import com.fasterxml.jackson.databind.DeserializationContext;
 import com.fasterxml.jackson.databind.JsonDeserializer;
 import com.fasterxml.jackson.databind.Module;
 import com.fasterxml.jackson.databind.module.SimpleModule;
+import com.google.common.annotations.VisibleForTesting;
+
 import org.apache.maven.artifact.versioning.ComparableVersion;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 import java.io.IOException;
+import java.net.URI;
+import java.net.URISyntaxException;
+import java.net.URL;
 
 @Configuration
 public class JacksonConfig {
@@ -18,6 +23,7 @@ public class JacksonConfig {
   public Module customDeserializers() {
     SimpleModule fafModule = new SimpleModule();
     fafModule.addDeserializer(ComparableVersion.class, new ComparableVersionDeserializer());
+    fafModule.addDeserializer(URL.class, new UrlDeserializer());
     return fafModule;
   }
 
@@ -26,6 +32,22 @@ public class JacksonConfig {
     @Override
     public ComparableVersion deserialize(JsonParser p, DeserializationContext ctxt) throws IOException {
       return new ComparableVersion(p.getValueAsString());
+    }
+  }
+
+  @VisibleForTesting
+  static class UrlDeserializer extends JsonDeserializer<URL> {
+
+    @Override
+    public URL deserialize(JsonParser p, DeserializationContext ctxt) throws IOException {
+      // the default URL parsing is bad, see https://stackoverflow.com/questions/10786042/java-url-encoding-of-query-string-parameters
+      URL url = new URL(p.getValueAsString());
+      try {
+        URI uri = new URI(url.getProtocol(), url.getUserInfo(), url.getHost(), url.getPort(), url.getPath(), url.getQuery(), url.getRef());
+        return uri.toURL();
+      } catch (URISyntaxException e) {
+        throw new IOException("Could not parse URL!", e);
+      }
     }
   }
 }
