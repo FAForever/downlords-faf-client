44274896505a329dda74b383b06dc29e3fe9859c
andreasley <h5gi925l>
2019-11-27 22:58:01 +0100

    https://github.com/FAForever/downlords-faf-client/pull/1507#discussion_r351187905

--- src/main/java/com/faforever/client/main/event/LocalReplaysChangedEvent.java
+++ src/main/java/com/faforever/client/main/event/LocalReplaysChangedEvent.java
@@ -1,11 +1,11 @@
 package com.faforever.client.main.event;
 
 import com.faforever.client.replay.Replay;
-import lombok.Data;
+import lombok.Value;
 
 import java.util.Collection;
 
-@Data
+@Value
 public class LocalReplaysChangedEvent {
   private final Collection<Replay> newReplays;
   private final Collection<Replay> deletedReplays;
