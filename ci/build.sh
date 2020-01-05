43dcbdb419b67c326e72b3ae01e15a43c5d2b761
Brutus5000 <Brutus5000@gmx.net>
2020-01-05 15:55:01 +0100

    Fix build dependency url for openjfx

--- ci/build.sh
+++ ci/build.sh
@@ -3,7 +3,7 @@
 # Download JavaFX only if it doesn't already exist (from Travis cache)
 if [ ! -d "$HOME/openjfx/javafx-sdk-11" ]; then
   mkdir -p "$HOME/openjfx"
-  curl http://content.faforever.com/openjfx/openjfx-11_linux-x64_bin-sdk.zip -o "$HOME/openjfx/openjfx.zip"
+  curl https://content.faforever.com/openjfx/openjfx-11_linux-x64_bin-sdk.zip -o "$HOME/openjfx/openjfx.zip"
   unzip -d "$HOME/openjfx" "$HOME/openjfx/openjfx.zip"
   ls -lah $HOME/openjfx/javafx-sdk-11/lib
 fi
