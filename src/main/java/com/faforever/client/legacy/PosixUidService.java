5044e8519064754d49694ff0014fa7f15d6d1a99
Alexander von Trostorff <19996713+axel1200@users.noreply.github.com>
2020-03-22 21:02:33 +0100

    Uid not found in folder with space (#1625)
    Also sets the executable flag when downloading the uid executable.
    Fixes #1624

--- src/main/java/com/faforever/client/legacy/PosixUidService.java
+++ src/main/java/com/faforever/client/legacy/PosixUidService.java
@@ -1,16 +1,13 @@
 package com.faforever.client.legacy;
 
 import com.faforever.client.os.OsUtils;
-import com.google.common.collect.Sets;
 import org.springframework.context.annotation.Lazy;
 import org.springframework.context.annotation.Profile;
 import org.springframework.stereotype.Service;
 
 import java.io.IOException;
-import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
-import java.nio.file.attribute.PosixFilePermission;
 
 
 @Lazy
@@ -22,6 +19,6 @@ public class PosixUidService implements UidService {
   public String generate(String sessionId, Path logFile) throws IOException {
     String uidDir = System.getProperty("nativeDir", "lib");
     Path uidPath = Paths.get(uidDir).resolve("faf-uid");
-    return OsUtils.execAndGetOutput(String.format("%s %s", uidPath.toAbsolutePath(), sessionId));
+    return OsUtils.execAndGetOutput(uidPath.toAbsolutePath().toString(), sessionId);
   }
 }
