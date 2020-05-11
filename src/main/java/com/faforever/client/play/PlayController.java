21f68ca0baa69675ca1374933af815c1972556c9
BlackYps <jonas-fb@gmx.de>
2020-05-11 19:01:08 +0200

    rearrange subtabs and highlight them more

--- src/main/java/com/faforever/client/play/PlayController.java
+++ src/main/java/com/faforever/client/play/PlayController.java
@@ -39,6 +39,7 @@ public class PlayController extends AbstractViewController<Node> {
 
   @Override
   public void initialize() {
+    eventBus.post(new OpenCustomGamesEvent());
     playRootTabPane.getSelectionModel().selectedItemProperty().addListener((observable, oldValue, newValue) -> {
       if (isHandlingEvent) {
         return;
@@ -59,8 +60,7 @@ public class PlayController extends AbstractViewController<Node> {
     isHandlingEvent = true;
 
     try {
-      if (Objects.equals(navigateEvent.getClass(), NavigateEvent.class)
-          || navigateEvent instanceof OpenCustomGamesEvent) {
+      if (navigateEvent instanceof OpenCustomGamesEvent) {
         playRootTabPane.getSelectionModel().select(customGamesTab);
         customGamesController.display(navigateEvent);
       }
@@ -75,13 +75,6 @@ public class PlayController extends AbstractViewController<Node> {
     } finally {
       isHandlingEvent = false;
     }
-  }
-
-  @Override
-  protected void onHide() {
-    customGamesController.hide();
-    ladderController.hide();
-    coopController.hide();
   }
 
   @Override
