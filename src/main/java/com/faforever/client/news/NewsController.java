f0d3013555f4e676d306702e52f84bc52873c930
BlackYps <jonas-fb@gmx.de>
2020-05-21 19:15:52 +0200

    load url from properties

--- src/main/java/com/faforever/client/news/NewsController.java
+++ src/main/java/com/faforever/client/news/NewsController.java
@@ -1,46 +1,37 @@
 package com.faforever.client.news;
 
+import com.faforever.client.config.ClientProperties;
+import com.faforever.client.config.ClientProperties.Website;
 import com.faforever.client.fx.AbstractViewController;
 import com.faforever.client.fx.WebViewConfigurer;
-import com.faforever.client.i18n.I18n;
 import com.faforever.client.main.event.NavigateEvent;
-import com.faforever.client.main.event.ShowLadderMapsEvent;
-import com.faforever.client.preferences.PreferencesService;
-import com.faforever.client.theme.UiService;
 import com.google.common.eventbus.EventBus;
 import com.google.common.eventbus.Subscribe;
-import com.google.common.io.CharStreams;
 import javafx.application.Platform;
 import javafx.beans.value.ChangeListener;
 import javafx.scene.Node;
-import javafx.scene.control.Button;
 import javafx.scene.control.Control;
-import javafx.scene.control.ListView;
 import javafx.scene.layout.Pane;
 import javafx.scene.web.WebView;
-import lombok.SneakyThrows;
 import org.springframework.beans.factory.config.ConfigurableBeanFactory;
 import org.springframework.context.annotation.Scope;
-import org.springframework.core.io.ClassPathResource;
 import org.springframework.stereotype.Component;
-
-import java.io.InputStreamReader;
-import java.io.Reader;
 
 @Component
 @Scope(ConfigurableBeanFactory.SCOPE_PROTOTYPE)
 public class NewsController extends AbstractViewController<Node> {
-
   private final EventBus eventBus;
   private final WebViewConfigurer webViewConfigurer;
+  private final ClientProperties clientProperties;
   public Pane newsRoot;
   public WebView newsWebView;
   public Control loadingIndicator;
   private ChangeListener<Boolean> loadingIndicatorListener;
 
-  public NewsController(EventBus eventBus, WebViewConfigurer webViewConfigurer) {
+  public NewsController(EventBus eventBus, WebViewConfigurer webViewConfigurer, ClientProperties clientProperties) {
     this.eventBus = eventBus;
     this.webViewConfigurer = webViewConfigurer;
+    this.clientProperties = clientProperties;
 
     loadingIndicatorListener = (observable, oldValue, newValue)
         -> loadingIndicator.getParent().getChildrenUnmodifiable().stream()
@@ -88,7 +79,8 @@ public class NewsController extends AbstractViewController<Node> {
 
   private void loadNews() {
     Platform.runLater(() -> {
-      newsWebView.getEngine().load("https://www.faforever.com/news");
+      Website website = clientProperties.getWebsite();
+      newsWebView.getEngine().load(website.getNewsHubUrl());
       onLoadingStop();
     });
   }
