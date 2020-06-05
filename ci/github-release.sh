74722b038c4144995c2068f358f09a34f9c885f5
axel12 <alexander.von.trostorff@gmail.com>
2020-06-05 20:30:12 +0200

    Update install4j download link

--- ci/github-release.sh
+++ ci/github-release.sh
@@ -5,7 +5,7 @@ INSTALL4J_DIR="$HOME/install4j/install4j7.0.12"
 if [ ! -d "${INSTALL4J_DIR}" ] || [ ! -d "${INSTALL4J_DIR}/jres/windows-amd64-11.0.7_packed.tar.gz" ]; then
   rm -rf "$HOME/install4j"
   mkdir -p "$HOME/install4j"
-  curl https://download-keycdn.ej-technologies.com/install4j/install4j_unix_7_0_12.tar.gz -o "$HOME/install4j/install4j.tar.gz"
+  curl https://download-gcdn.ej-technologies.com/install4j/install4j_unix_7_0_12.tar.gz -o "$HOME/install4j/install4j.tar.gz"
   mkdir -p "${INSTALL4J_DIR}/jres/"
   curl https://content.faforever.com/jre/windows-amd64-11.0.7.tar.gz -o "${INSTALL4J_DIR}/jres/windows-amd64-11.0.7_packed.tar.gz"
   tar xzf "$HOME/install4j/install4j.tar.gz" -C "$HOME/install4j"
