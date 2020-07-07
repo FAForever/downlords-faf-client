74528e21d407938e09de9b1d6259622cee7cc836
frau <frau@uni-bonn.de>
2020-07-07 15:22:21 +0200

    Added classes for rating with rank

---
+++ src/main/java/com/faforever/client/api/dto/GlobalRatingWithRank.java
@@ -0,0 +1,8 @@
+package com.faforever.client.api.dto;
+
+import com.github.jasminb.jsonapi.annotations.Type;
+
+@Type("globalRatingWithRank")
+public class GlobalRatingWithRank extends RatingWithRank {
+
+}
---
+++ src/main/java/com/faforever/client/api/dto/Ladder1v1RatingWithRank.java
@@ -0,0 +1,8 @@
+package com.faforever.client.api.dto;
+
+import com.github.jasminb.jsonapi.annotations.Type;
+
+@Type("Ladder1v1RatingWithRank")
+public class Ladder1v1RatingWithRank extends RatingWithRank {
+
+}
