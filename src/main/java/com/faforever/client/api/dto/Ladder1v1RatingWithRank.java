2595d49b63dfc2269f16db9671de85002b6aec34
F.S <s6fescha@uni-bonn.de>
2020-07-15 13:09:35 +0200

    Combined Ladder1v1RatingWithRank.java and GlobalRatingWithRank.java to parent class dto.RatingWithRank. Imported: Changed methods to accept 1v1 and global types now.

--- src/main/java/com/faforever/client/api/dto/Ladder1v1RatingWithRank.java
+++ src/main/java/com/faforever/client/api/dto/Ladder1v1RatingWithRank.java
@@ -5,16 +5,7 @@ import com.github.jasminb.jsonapi.annotations.Relationship;
 import com.github.jasminb.jsonapi.annotations.Type;
 import lombok.Data;
 
-@Data
-@Type("Ladder1v1RatingWithRank")
-public class Ladder1v1RatingWithRank {
-  @Id
-  private String id;
-  private double mean;
-  private double deviation;
-  private double rating;
-  private int rank;
 
-  @Relationship("player")
-  private Player player;
+@Type("Ladder1v1RatingWithRank")
+public class Ladder1v1RatingWithRank extends RatingWithRank {
 }
---
+++ src/main/java/com/faforever/client/api/dto/RatingWithRank.java
@@ -0,0 +1,22 @@
+package com.faforever.client.api.dto;
+
+import com.github.jasminb.jsonapi.annotations.Id;
+import com.github.jasminb.jsonapi.annotations.Relationship;
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+@EqualsAndHashCode(of = "id")
+public class RatingWithRank {
+  @Id
+  private String id;
+  private double mean;
+  private double deviation;
+  private double rating;
+  private int rank;
+
+  @Relationship("player")
+  private Player player;
+}
