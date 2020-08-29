7f1f4b36d0a3e7a1958b7a8ef9a9cdd8ce41849b
Sheikah45 <Sheikah450@gmail.com>
2020-08-29 09:39:03 -0400

    Store up to 10 game logs with time stamp

--- src/main/java/com/faforever/client/preferences/PreferencesService.java
+++ src/main/java/com/faforever/client/preferences/PreferencesService.java
@@ -9,6 +9,7 @@ import com.faforever.client.preferences.gson.PropertyTypeAdapter;
 import com.faforever.client.remote.gson.FactionTypeAdapter;
 import com.faforever.client.update.ClientConfiguration;
 import com.github.nocatch.NoCatch.NoCatchRunnable;
+import com.github.nocatch.NoCatchException;
 import com.google.gson.Gson;
 import com.google.gson.GsonBuilder;
 import com.sun.jna.platform.win32.Shell32Util;
@@ -47,13 +48,19 @@ import java.security.DigestInputStream;
 import java.security.MessageDigest;
 import java.security.NoSuchAlgorithmException;
 import java.text.MessageFormat;
+import java.text.SimpleDateFormat;
 import java.util.ArrayList;
 import java.util.Collection;
+import java.util.Collections;
+import java.util.Comparator;
+import java.util.Date;
 import java.util.Timer;
 import java.util.TimerTask;
 import java.util.concurrent.CompletableFuture;
 import java.util.concurrent.CompletionException;
 import java.util.concurrent.CountDownLatch;
+import java.util.regex.Pattern;
+import java.util.stream.Stream;
 
 import static com.github.nocatch.NoCatch.noCatch;
 
@@ -80,6 +87,8 @@ public class PreferencesService implements InitializingBean {
   private static final String CACHE_SUB_FOLDER = "cache";
   private static final String CACHE_STYLESHEETS_SUB_FOLDER = Paths.get(CACHE_SUB_FOLDER, "stylesheets").toString();
   private static final Path CACHE_DIRECTORY;
+  private static final Pattern GAME_LOG_PATTERN = Pattern.compile("game(_\\d{4}-\\d{2}-\\d{2}_\\d{6})?.log");
+  private static final int NUMBER_GAME_LOGS_STORED = 10;
 
   static {
     if (org.bridj.Platform.isWindows()) {
@@ -344,6 +353,22 @@ public class PreferencesService implements InitializingBean {
 
   public Path getThemesDirectory() {
     return getFafDataDirectory().resolve("themes");
+  }
+
+  public Path getNewGameLogFile() {
+    try (Stream<Path> listOfLogFiles = Files.list(getFafLogDirectory())) {
+      listOfLogFiles
+          .filter(p -> GAME_LOG_PATTERN.matcher(p.getFileName().toString()).matches())
+          .sorted(Comparator.comparingLong(p -> p.toFile().lastModified()))
+          .sorted(Collections.reverseOrder())
+          .skip(NUMBER_GAME_LOGS_STORED - 1)
+          .forEach(p -> noCatch(() -> Files.delete(p)));
+    } catch (IOException e) {
+      logger.error("Could not list log directory.", e);
+    } catch (NoCatchException e) {
+      logger.error("Could not delete game log file");
+    }
+    return getFafLogDirectory().resolve(new SimpleDateFormat("'game_'yyyy-MM-dd_HHmmss'.log'").format(new Date()));
   }
 
   @SneakyThrows
