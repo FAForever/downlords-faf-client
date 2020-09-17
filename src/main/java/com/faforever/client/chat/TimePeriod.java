5bfe39f7335bb628516d64231aed0e5c6d59f3eb
Sheikah45 <66929319+Sheikah45@users.noreply.github.com>
2020-09-17 04:56:53 -0400

    Add proper time filtering to rating graph and display year
    Fixes #935
    Fixes #1220
    
    Co-authored-by: axel1200 <alexander.von.trostorff@gmail.com>

---
+++ src/main/java/com/faforever/client/chat/TimePeriod.java
@@ -0,0 +1,21 @@
+package com.faforever.client.chat;
+
+import lombok.Getter;
+
+import java.time.LocalDateTime;
+
+public enum TimePeriod {
+  ALL_TIME("userInfo.ratingHistory.allTime", LocalDateTime.MIN),
+  LAST_YEAR("userInfo.ratingHistory.lastYear", LocalDateTime.now().minusYears(1)),
+  LAST_MONTH("userInfo.ratingHistory.lastMonth", LocalDateTime.now().minusMonths(1));
+
+  @Getter
+  private final String i18nKey;
+  @Getter
+  private final LocalDateTime date;
+
+  TimePeriod(String i18nKey, LocalDateTime date) {
+    this.i18nKey = i18nKey;
+    this.date = date;
+  }
+}
