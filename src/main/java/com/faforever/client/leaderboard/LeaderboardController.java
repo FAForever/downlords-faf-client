10fc5ac20ff2807e0adfb79a4088381fa5daec06
BlackYps <jonas-fb@gmx.de>
2020-09-17 21:36:56 +0200

    League Leaderboard mockup

--- src/main/java/com/faforever/client/leaderboard/LeaderboardController.java
+++ src/main/java/com/faforever/client/leaderboard/LeaderboardController.java
@@ -1,21 +1,35 @@
 package com.faforever.client.leaderboard;
 
 import com.faforever.client.fx.AbstractViewController;
+import com.faforever.client.fx.JavaFxUtil;
 import com.faforever.client.fx.StringCell;
 import com.faforever.client.game.KnownFeaturedMod;
 import com.faforever.client.i18n.I18n;
 import com.faforever.client.main.event.NavigateEvent;
 import com.faforever.client.notification.ImmediateErrorNotification;
 import com.faforever.client.notification.NotificationService;
+import com.faforever.client.player.Player;
+import com.faforever.client.player.PlayerService;
 import com.faforever.client.reporting.ReportingService;
 import com.faforever.client.util.Assert;
+import com.faforever.client.util.RatingUtil;
 import com.faforever.client.util.Validator;
+import javafx.application.Platform;
+import javafx.beans.InvalidationListener;
+import javafx.beans.WeakInvalidationListener;
 import javafx.beans.property.SimpleFloatProperty;
+import javafx.collections.ObservableList;
+import javafx.css.PseudoClass;
+import javafx.scene.Group;
 import javafx.scene.Node;
+import javafx.scene.chart.BarChart;
+import javafx.scene.chart.XYChart;
 import javafx.scene.control.TableColumn;
 import javafx.scene.control.TableView;
 import javafx.scene.control.TextField;
 import javafx.scene.layout.Pane;
+import javafx.scene.layout.VBox;
+import javafx.scene.text.Text;
 import lombok.RequiredArgsConstructor;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -24,6 +38,9 @@ import org.springframework.context.annotation.Scope;
 import org.springframework.stereotype.Component;
 
 import java.lang.invoke.MethodHandles;
+import java.util.Comparator;
+import java.util.List;
+import java.util.stream.Collectors;
 
 import static javafx.collections.FXCollections.observableList;
 
@@ -34,21 +51,27 @@ import static javafx.collections.FXCollections.observableList;
 public class LeaderboardController extends AbstractViewController<Node> {
 
   private static final Logger logger = LoggerFactory.getLogger(MethodHandles.lookup().lookupClass());
+  private static final PseudoClass NOTIFICATION_HIGHLIGHTED_PSEUDO_CLASS = PseudoClass.getPseudoClass("highlighted-bar");
+
   private final LeaderboardService leaderboardService;
   private final NotificationService notificationService;
   private final I18n i18n;
   private final ReportingService reportingService;
-  public Pane leaderboardRoot;
+  private final PlayerService playerService;
+  public VBox leaderboardRoot;
   public TableColumn<LeaderboardEntry, Number> rankColumn;
   public TableColumn<LeaderboardEntry, String> nameColumn;
-  public TableColumn<LeaderboardEntry, Number> winLossColumn;
+  public TableColumn<LeaderboardEntry, Number> divisionColumn;
   public TableColumn<LeaderboardEntry, Number> gamesPlayedColumn;
-  public TableColumn<LeaderboardEntry, Number> ratingColumn;
+  public TableColumn<LeaderboardEntry, Number> scoreColumn;
   public TableView<LeaderboardEntry> ratingTable;
   public TextField searchTextField;
   public Pane connectionProgressPane;
   public Pane contentPane;
+  public BarChart ratingDistributionChart;
   private KnownFeaturedMod ratingType;
+  private Text youLabel;
+  private InvalidationListener playerRatingListener;
 
   @Override
   public void initialize() {
@@ -59,18 +82,24 @@ public class LeaderboardController extends AbstractViewController<Node> {
     nameColumn.setCellValueFactory(param -> param.getValue().usernameProperty());
     nameColumn.setCellFactory(param -> new StringCell<>(name -> name));
 
-    winLossColumn.setCellValueFactory(param -> new SimpleFloatProperty(param.getValue().getWinLossRatio()));
-    winLossColumn.setCellFactory(param -> new StringCell<>(number -> i18n.get("percentage", number.floatValue() * 100)));
+    divisionColumn.setCellValueFactory(param -> new SimpleFloatProperty(param.getValue().getWinLossRatio()));
+    divisionColumn.setCellFactory(param -> new StringCell<>(number -> i18n.get("percentage", number.floatValue() * 100)));
 
     gamesPlayedColumn.setCellValueFactory(param -> param.getValue().gamesPlayedProperty());
     gamesPlayedColumn.setCellFactory(param -> new StringCell<>(count -> i18n.number(count.intValue())));
 
-    ratingColumn.setCellValueFactory(param -> param.getValue().ratingProperty());
-    ratingColumn.setCellFactory(param -> new StringCell<>(rating -> i18n.number(rating.intValue())));
+    scoreColumn.setCellValueFactory(param -> param.getValue().ratingProperty());
+    scoreColumn.setCellFactory(param -> new StringCell<>(rating -> i18n.number(rating.intValue())));
 
     contentPane.managedProperty().bind(contentPane.visibleProperty());
     connectionProgressPane.managedProperty().bind(connectionProgressPane.visibleProperty());
     connectionProgressPane.visibleProperty().bind(contentPane.visibleProperty().not());
+
+    youLabel = new Text("IV");
+    youLabel.setId("1v1-you-text");
+
+    JavaFxUtil.addListener(playerService.currentPlayerProperty(), (observable, oldValue, newValue) -> Platform.runLater(() -> setCurrentPlayer(newValue)));
+    playerService.getCurrentPlayer().ifPresent(this::setCurrentPlayer);
 
     searchTextField.textProperty().addListener((observable, oldValue, newValue) -> {
       if (Validator.isInt(newValue)) {
@@ -127,4 +156,74 @@ public class LeaderboardController extends AbstractViewController<Node> {
   public void setRatingType(KnownFeaturedMod ratingType) {
     this.ratingType = ratingType;
   }
+
+  private void setCurrentPlayer(Player player) {
+    playerRatingListener = ratingObservable -> Platform.runLater(() -> updateRating(player));
+
+    JavaFxUtil.addListener(player.leaderboardRatingDeviationProperty(), new WeakInvalidationListener(playerRatingListener));
+    JavaFxUtil.addListener(player.leaderboardRatingMeanProperty(), new WeakInvalidationListener(playerRatingListener));
+    updateRating(player);
+  }
+
+  private void updateRating(Player player) {
+
+    leaderboardService.getLadder1v1Stats()
+        .thenAccept(ranked1v1Stats -> {
+          ranked1v1Stats.sort(Comparator.comparingInt(RatingStat::getRating));
+          int totalPlayers = 0;
+          for (RatingStat entry : ranked1v1Stats) {
+            totalPlayers += entry.getTotalCount();
+          }
+          plotRatingDistributions(ranked1v1Stats, player);
+          String rankingOutOfText = i18n.get("ranked1v1.rankingOutOf", totalPlayers);
+        })
+        .exceptionally(throwable -> {
+          logger.warn("Could not plot rating distribution", throwable);
+          return null;
+        });
+  }
+
+  private void plotRatingDistributions(List<RatingStat> ratingStats, Player player) {
+    XYChart.Series<String, Integer> series = new XYChart.Series<>();
+    series.setName(i18n.get("ranked1v1.players", LeaderboardService.MINIMUM_GAMES_PLAYED_TO_BE_SHOWN));
+    int currentPlayerRating = RatingUtil.roundRatingToNextLowest100(RatingUtil.getLeaderboardRating(player));
+
+    series.getData().addAll(ratingStats.stream()
+        .sorted(Comparator.comparingInt(RatingStat::getRating))
+        .map(item -> {
+          int rating = item.getRating();
+          XYChart.Data<String, Integer> data = new XYChart.Data<>(i18n.number(rating), item.getCountWithEnoughGamesPlayed());
+          if (rating == currentPlayerRating) {
+            data.nodeProperty().addListener((observable, oldValue, newValue) -> {
+              newValue.pseudoClassStateChanged(NOTIFICATION_HIGHLIGHTED_PSEUDO_CLASS, true);
+              addNodeOnTopOfBar(data, youLabel);
+            });
+          }
+          return data;
+        })
+        .collect(Collectors.toList()));
+
+    Platform.runLater(() -> ratingDistributionChart.getData().setAll(series));
+  }
+
+  private void addNodeOnTopOfBar(XYChart.Data<String, Integer> data, Node nodeToAdd) {
+    final Node node = data.getNode();
+    node.parentProperty().addListener((ov, oldParent, parent) -> {
+      if (parent == null) {
+        return;
+      }
+      Group parentGroup = (Group) parent;
+      ObservableList<Node> children = parentGroup.getChildren();
+      if (!children.contains(nodeToAdd)) {
+        children.add(nodeToAdd);
+        nodeToAdd.setViewOrder(-0.5);
+      }
+    });
+
+    JavaFxUtil.addListener(node.boundsInParentProperty(), (ov, oldBounds, bounds) -> {
+      nodeToAdd.setLayoutX(Math.round(bounds.getMinX() + bounds.getWidth() / 2 - nodeToAdd.prefWidth(-1) / 2));
+      nodeToAdd.setLayoutY(Math.round(bounds.getMaxY() - nodeToAdd.prefHeight(-1) * 0.5));
+    });
+  }
+
 }
