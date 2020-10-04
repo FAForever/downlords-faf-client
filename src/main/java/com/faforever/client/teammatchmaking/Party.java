ec2f619e39d36454b9102e07e9490f15a92680a7
Geosearchef <geosearchef@googlemail.com>
2020-10-04 21:31:35 +0200

    select all factions in an empty party

--- src/main/java/com/faforever/client/teammatchmaking/Party.java
+++ src/main/java/com/faforever/client/teammatchmaking/Party.java
@@ -13,6 +13,7 @@ import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.extern.slf4j.Slf4j;
 
+import java.util.Arrays;
 import java.util.Collections;
 import java.util.List;
 import java.util.Objects;
@@ -91,7 +92,7 @@ public class Party {
     public PartyMember(Player player) {
       this.player = player;
       this.ready = false;
-      this.factions = Collections.emptyList();
+      this.factions = Arrays.asList(Faction.AEON, Faction.CYBRAN, Faction.SERAPHIM, Faction.UEF);
     }
   }
 }
