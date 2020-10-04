54792c6e301c042d8ba184962b29e2537fa1b362
Geosearchef <geosearchef@googlemail.com>
2020-10-04 19:03:27 +0200

    unbump install4j version (to 7, use different key)

--- ci/github-release.sh
+++ ci/github-release.sh
@@ -1,35 +1,35 @@
 #!/usr/bin/env bash
 
 # Download install4j and the required JRE, only if it doesn't already exist (from Travis cache)
-INSTALL4J_DIR="$HOME/install4j/install4j8.0.8"
-if [ ! -d "${INSTALL4J_DIR}" ] || [ ! -d "$HOME/.install4j8/jres/windows-amd64-10.0.2.tar.gz" ]; then
-  rm -rf "$HOME/install4j"
-  mkdir -p "$HOME/install4j"
-  curl https://download-gcdn.ej-technologies.com/install4j/install4j_unix_8_0_8.tar.gz -o "$HOME/install4j/install4j8.tar.gz"
-  mkdir -p "$HOME/.install4j8/jres/"
-  curl https://content.faforever.com/jre/windows-amd64-10.0.2.tar.gz -o "$HOME/.install4j8/jres/windows-amd64-10.0.2.tar.gz"
-  tar xzf "$HOME/install4j/install4j8.tar.gz" -C "$HOME/install4j"
-fi
-
-#INSTALL4J_DIR="$HOME/install4j/install4j7.0.12"
-#if [ ! -d "${INSTALL4J_DIR}" ]; then
+#INSTALL4J_DIR="$HOME/install4j/install4j8.0.8"
+#if [ ! -d "${INSTALL4J_DIR}" ] || [ ! -d "$HOME/.install4j8/jres/windows-amd64-10.0.2.tar.gz" ]; then
+#  rm -rf "$HOME/install4j"
 #  mkdir -p "$HOME/install4j"
-#  curl https://download-gcdn.ej-technologies.com/install4j/install4j_unix_7_0_12.tar.gz -o "$HOME/install4j/install4j.tar.gz"
-#  mkdir -p "${INSTALL4J_DIR}/jres/"
-#  curl https://content.faforever.com/jre/windows-amd64-10.0.2.tar.gz -o "${INSTALL4J_DIR}/jres/windows-amd64-10.0.2.tar.gz"
-#  tar xzf "$HOME/install4j/install4j.tar.gz" -C "$HOME/install4j"
+#  curl https://download-gcdn.ej-technologies.com/install4j/install4j_unix_8_0_8.tar.gz -o "$HOME/install4j/install4j8.tar.gz"
+#  mkdir -p "$HOME/.install4j8/jres/"
+#  curl https://content.faforever.com/jre/windows-amd64-10.0.2.tar.gz -o "$HOME/.install4j8/jres/windows-amd64-10.0.2.tar.gz"
+#  tar xzf "$HOME/install4j/install4j8.tar.gz" -C "$HOME/install4j"
 #fi
+
+INSTALL4J_DIR="$HOME/install4j/install4j7.0.12"
+if [ ! -d "${INSTALL4J_DIR}" ]; then
+  mkdir -p "$HOME/install4j"
+  curl https://download-gcdn.ej-technologies.com/install4j/install4j_unix_7_0_12.tar.gz -o "$HOME/install4j/install4j.tar.gz"
+  mkdir -p "${INSTALL4J_DIR}/jres/"
+  curl https://content.faforever.com/jre/windows-amd64-10.0.2.tar.gz -o "${INSTALL4J_DIR}/jres/windows-amd64-10.0.2.tar.gz"
+  tar xzf "$HOME/install4j/install4j.tar.gz" -C "$HOME/install4j"
+fi
 
 ./gradlew -Pversion=${APP_VERSION} \
   -PjavafxPlatform=linux \
   -Pinstall4jHomeDir="${INSTALL4J_DIR}" \
-  -Pinstall4jLicense=${install4jLicense} \
+  -Pinstall4jLicense=${install4j7License} \
   buildInstall4jMediaFiles
 
 ./gradlew -Pversion=${APP_VERSION} \
   -PjavafxPlatform=win \
   -Pinstall4jHomeDir="${INSTALL4J_DIR}" \
-  -Pinstall4jLicense=${install4jLicense} \
+  -Pinstall4jLicense=${install4j7License} \
   --info \
   --stacktrace \
   buildInstall4jMediaFiles
@@ -37,7 +37,7 @@ fi
 ./gradlew -Pversion=${APP_VERSION} \
   -PjavafxPlatform=mac \
   -Pinstall4jHomeDir="${INSTALL4J_DIR}" \
-  -Pinstall4jLicense=${install4jLicense} \
+  -Pinstall4jLicense=${install4j7License} \
   buildInstall4jMediaFiles
 
 RELEASE_BODY=$(python3 ./ci/release-body.py ${GITHUB_RELEASE_VERSION})
