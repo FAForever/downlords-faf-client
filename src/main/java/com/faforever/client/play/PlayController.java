abcc424da25ab9251ad1430026c6a3bedf8a3739
BlackYps <jonas-fb@gmx.de>
2020-11-11 11:05:57 +0100

    Remove ladder tab

--- src/main/java/com/faforever/client/play/PlayController.java
+++ src/main/java/com/faforever/client/play/PlayController.java
@@ -4,11 +4,9 @@ import com.faforever.client.coop.CoopController;
 import com.faforever.client.fx.AbstractViewController;
 import com.faforever.client.game.CustomGamesController;
 import com.faforever.client.main.event.NavigateEvent;
-import com.faforever.client.main.event.Open1v1Event;
 import com.faforever.client.main.event.OpenCoopEvent;
 import com.faforever.client.main.event.OpenCustomGamesEvent;
 import com.faforever.client.main.event.OpenTeamMatchmakingEvent;
-import com.faforever.client.rankedmatch.Ladder1v1Controller;
 import com.faforever.client.teammatchmaking.TeamMatchmakingController;
 import com.google.common.eventbus.EventBus;
 import javafx.scene.Node;
@@ -17,8 +15,6 @@ import javafx.scene.control.TabPane;
 import org.springframework.beans.factory.config.ConfigurableBeanFactory;
 import org.springframework.context.annotation.Scope;
 import org.springframework.stereotype.Component;
-
-import java.util.Objects;
 
 @Component
 @Scope(ConfigurableBeanFactory.SCOPE_PROTOTYPE)
@@ -32,7 +28,6 @@ public class PlayController extends AbstractViewController<Node> {
   public TabPane playRootTabPane;
   public TeamMatchmakingController teamMatchmakingController;
   public CustomGamesController customGamesController;
-  public Ladder1v1Controller ladderController;
   public CoopController coopController;
   private boolean isHandlingEvent;
   private AbstractViewController<?> lastTabController;
@@ -55,8 +50,6 @@ public class PlayController extends AbstractViewController<Node> {
         eventBus.post(new OpenTeamMatchmakingEvent());
       } else if (newValue == customGamesTab) {
         eventBus.post(new OpenCustomGamesEvent());
-      } else if (newValue == ladderTab) {
-        eventBus.post(new Open1v1Event());
       } else if (newValue == coopTab) {
         eventBus.post(new OpenCoopEvent());
       }
@@ -74,9 +67,6 @@ public class PlayController extends AbstractViewController<Node> {
       } else if (navigateEvent instanceof OpenCustomGamesEvent) {
         lastTab = customGamesTab;
         lastTabController = customGamesController;
-      } else if (navigateEvent instanceof Open1v1Event) {
-        lastTab = ladderTab;
-        lastTabController = ladderController;
       } else if (navigateEvent instanceof OpenCoopEvent) {
         lastTab = coopTab;
         lastTabController = coopController;
@@ -92,7 +82,6 @@ public class PlayController extends AbstractViewController<Node> {
   public void onHide() {
     teamMatchmakingController.onHide();
     customGamesController.onHide();
-    ladderController.onHide();
     coopController.onHide();
   }
 
