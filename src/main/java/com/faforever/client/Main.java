d4bedc316363b275567db167f55e3fe340d72cf3
Jan <katharsas@users.noreply.github.com>
2020-12-24 03:46:08 +0100

    Fix for NullPointerException (see #1972)

--- src/main/java/com/faforever/client/Main.java
+++ src/main/java/com/faforever/client/Main.java
@@ -4,12 +4,15 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.jmx.JmxAutoConfiguration;
 import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
 
+import javafx.fxml.FXMLLoader;
+
 @SpringBootApplication(exclude = {
     JmxAutoConfiguration.class,
     SecurityAutoConfiguration.class,
 })
 public class Main {
-	public static void main(String[] args) {
-		FafClientApplication.applicationMain(args);
-	}
+  public static void main(String[] args) {
+    FXMLLoader.setDefaultClassLoader(Main.class.getClassLoader());
+    FafClientApplication.applicationMain(args);
+  }
 }
