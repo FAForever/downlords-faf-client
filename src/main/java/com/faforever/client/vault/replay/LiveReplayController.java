0b21de704af5d58e3bc5facba73ff9d59f5b3cf6
Sheikah45 <Sheikah450@gmail.com>
2021-02-21 09:56:40 -0500

    Fixes #2157

--- src/main/java/com/faforever/client/vault/replay/LiveReplayController.java
+++ src/main/java/com/faforever/client/vault/replay/LiveReplayController.java
@@ -127,7 +127,7 @@ public class LiveReplayController extends AbstractViewController<Node> {
         .map(Entry::getValue)
         .collect(Collectors.toList());
     if (simModCount > 2) {
-      return new SimpleStringProperty(i18n.get("game.mods.twoAndMore", modNames.get(0), modNames.size()));
+      return new SimpleStringProperty(i18n.get("game.mods.twoAndMore", modNames.get(0), modNames.get(1), simMods.size() - 2));
     }
     return new SimpleStringProperty(Joiner.on(i18n.get("textSeparator")).join(modNames));
   }
