bfd83e607658fedfa53ec3779c15c8c21f274674
axel12 <alexander.von.trostorff@gmail.com>
2021-03-08 00:42:00 +0100

    Rework invite ui
    Fixes #2181

---
+++ src/test/java/com/faforever/client/teammatchmaking/InvitePlayerControllerTest.java
@@ -0,0 +1,45 @@
+package com.faforever.client.teammatchmaking;
+
+import com.faforever.client.player.Player;
+import com.faforever.client.player.PlayerService;
+import com.faforever.client.test.AbstractPlainJavaFxTest;
+import com.faforever.client.theme.UiService;
+import org.junit.Before;
+import org.junit.Test;
+import org.mockito.Mock;
+
+import java.io.IOException;
+import java.util.Optional;
+import java.util.Set;
+
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.hasSize;
+import static org.mockito.Mockito.when;
+
+public class InvitePlayerControllerTest extends AbstractPlainJavaFxTest {
+
+  @Mock
+  private PlayerService playerService;
+  @Mock
+  private TeamMatchmakingService teamMatchmakingService;
+  @Mock
+  private UiService uiService;
+
+  private InvitePlayerController instance;
+
+  @Before
+  public void setUp() throws IOException {
+    when(playerService.getPlayerNames())
+        .thenReturn(Set.of("axel12", "TrustTheFall", "nInPrisonForWhat", "Sheikah"));
+    when(playerService.getCurrentPlayer())
+        .thenReturn(Optional.of(new Player("axel12")));
+    instance = new InvitePlayerController(playerService, uiService, teamMatchmakingService);
+    loadFxml("theme/play/teammatchmaking/matchmaking_invite_player.fxml", clazz -> instance);
+  }
+
+  @Test
+  public void testGivenInviteUiWhenFilterIsFilledOutShouldShowUsersToInviteThatMatchFilter() {
+    instance.playerTextField.setText("T");
+    assertThat(instance.playersListView.getItems(), hasSize(2));
+  }
+}
