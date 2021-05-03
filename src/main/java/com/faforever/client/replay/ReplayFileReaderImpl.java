80fdbc8f8cd43c422fd0493c35ce439224c5318c
Sheikah45 <Sheikah450@gmail.com>
2021-05-03 10:27:46 -0400

    update commons version

--- src/main/java/com/faforever/client/replay/ReplayFileReaderImpl.java
+++ src/main/java/com/faforever/client/replay/ReplayFileReaderImpl.java
@@ -2,6 +2,8 @@ package com.faforever.client.replay;
 
 import com.faforever.commons.replay.ReplayData;
 import com.faforever.commons.replay.ReplayDataParser;
+import com.fasterxml.jackson.databind.DeserializationFeature;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.context.annotation.Lazy;
 import org.springframework.stereotype.Component;
@@ -13,8 +15,10 @@ import java.nio.file.Path;
 @Slf4j
 public class ReplayFileReaderImpl implements ReplayFileReader {
 
+  private final ObjectMapper objectMapper = new ObjectMapper().configure(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES, false);
+
   @Override
   public ReplayData parseReplay(Path path) {
-    return new ReplayDataParser(path).parse();
+    return new ReplayDataParser(path, objectMapper).parse();
   }
 }
