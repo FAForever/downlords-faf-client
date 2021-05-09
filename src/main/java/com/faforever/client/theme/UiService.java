e3271a16465dd70926d5a5a3a74aaf9546696429
ivan-gryzunov <38242291+ivan-gryzunov@users.noreply.github.com>
2021-05-09 18:09:01 +0300

    #2217 - Make "Upload map" dialog as scrollable (#2221)
    Co-authored-by: Ivan <v23620@gmail.com>

--- src/main/java/com/faforever/client/theme/UiService.java
+++ src/main/java/com/faforever/client/theme/UiService.java
@@ -21,8 +21,11 @@ import javafx.scene.Node;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.control.Label;
+import javafx.scene.control.ScrollPane;
+import javafx.scene.control.ScrollPane.ScrollBarPolicy;
 import javafx.scene.image.Image;
 import javafx.scene.input.KeyCode;
+import javafx.scene.layout.Region;
 import javafx.scene.layout.StackPane;
 import javafx.scene.web.WebView;
 import lombok.SneakyThrows;
@@ -502,6 +505,14 @@ public class UiService implements InitializingBean, DisposableBean {
 
     dialog.show(parent);
     return dialog;
+  }
+
+  public void makeScrollableDialog(Dialog dialog) {
+    Region dialogContent = dialog.getContent();
+    ScrollPane scrollPane = new ScrollPane(dialogContent);
+    scrollPane.setHbarPolicy(ScrollBarPolicy.NEVER);
+    JavaFxUtil.bind(scrollPane.prefHeightProperty(), dialogContent.heightProperty());
+    dialog.setContent(scrollPane);
   }
 
   @SneakyThrows
