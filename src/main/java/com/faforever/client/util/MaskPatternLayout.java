4fb4407f549de2935fa60ea06530a599b6fbba2e
Sheikah45 <Sheikah450@gmail.com>
2021-07-11 12:11:39 -0400

    Mask user details in log file

---
+++ src/main/java/com/faforever/client/util/MaskPatternLayout.java
@@ -0,0 +1,41 @@
+package com.faforever.client.util;
+
+import ch.qos.logback.classic.PatternLayout;
+import ch.qos.logback.classic.spi.ILoggingEvent;
+
+import java.net.InetAddress;
+import java.net.UnknownHostException;
+
+public class MaskPatternLayout extends PatternLayout {
+  private final String userProfile;
+  private final String machineName;
+  private final String user;
+
+  public MaskPatternLayout() {
+    if (org.bridj.Platform.isWindows()) {
+      userProfile = System.getenv("USERPROFILE");
+    } else {
+      userProfile = System.getProperty("user.home");
+    }
+    user = System.getProperty("user.name");
+    String machineName;
+    try {
+      machineName = InetAddress.getLocalHost().getHostName();
+    } catch (UnknownHostException e) {
+      machineName = "";
+    }
+    this.machineName = machineName;
+  }
+
+  @Override
+  public String doLayout(ILoggingEvent event) {
+    return maskMessage(super.doLayout(event));
+  }
+
+  private String maskMessage(String message) {
+    return message
+        .replace(userProfile, "{USER_PROFILE}")
+        .replace(machineName, "{DEVICE_NAME}")
+        .replace(user, "{USER}");
+  }
+}
