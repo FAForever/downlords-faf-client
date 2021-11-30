da0dd4de8ac29ac9ce21288c863a272e49555d99
Ivan <v23620@gmail.com>
2021-12-01 00:53:21 +0300

    show attribution

--- src/main/java/com/faforever/client/chat/EmoticonsGroupController.java
+++ src/main/java/com/faforever/client/chat/EmoticonsGroupController.java
@@ -2,13 +2,18 @@ package com.faforever.client.chat;
 
 import com.faforever.client.chat.emoticons.EmoticonsGroup;
 import com.faforever.client.fx.Controller;
+import com.faforever.client.fx.JavaFxUtil;
+import com.faforever.client.fx.PlatformService;
 import com.faforever.client.theme.UiService;
+import javafx.scene.control.Hyperlink;
 import javafx.scene.control.Label;
 import javafx.scene.layout.AnchorPane;
 import javafx.scene.layout.FlowPane;
+import javafx.scene.layout.HBox;
 import javafx.scene.layout.VBox;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.apache.commons.lang3.StringUtils;
 import org.springframework.beans.factory.config.ConfigurableBeanFactory;
 import org.springframework.context.annotation.Scope;
 import org.springframework.stereotype.Component;
@@ -24,17 +29,27 @@ import java.util.stream.Collectors;
 public class EmoticonsGroupController implements Controller<VBox> {
 
   private final UiService uiService;
+  private final PlatformService platformService;
 
   public VBox root;
   public Label groupLabel;
+  public HBox attributionPane;
+  public Hyperlink attributionHyperlink;
   public FlowPane emoticonsPane;
 
   @Override
   public void initialize() {
+    JavaFxUtil.bindManagedToVisible(attributionPane);
   }
 
   public void setGroup(EmoticonsGroup group, Consumer<String> onEmoticonAction) {
     groupLabel.setText(group.getName());
+    String attribution = group.getAttribution();
+    if (!StringUtils.isBlank(attribution)) {
+      attributionHyperlink.setText(attribution);
+      attributionHyperlink.setOnAction(event -> platformService.showDocument(attribution));
+      attributionPane.setVisible(true);
+    }
     List<AnchorPane> emoticonViewList = group.getEmoticons().stream().map(emoticon -> {
       EmoticonController controller = uiService.loadFxml("theme/chat/emoticon/emoticon.fxml");
       controller.setEmoticon(emoticon, onEmoticonAction);
