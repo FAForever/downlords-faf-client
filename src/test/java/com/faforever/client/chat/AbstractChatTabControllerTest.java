a75f97d828af561e05201e19a3dc03cd5b8fda40
Ivan <v23620@gmail.com>
2021-12-02 02:43:20 +0300

    fix codacy issues

--- src/test/java/com/faforever/client/chat/AbstractChatTabControllerTest.java
+++ src/test/java/com/faforever/client/chat/AbstractChatTabControllerTest.java
@@ -61,7 +61,6 @@ import static org.hamcrest.Matchers.isEmptyString;
 import static org.hamcrest.core.Is.is;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertFalse;
-import static org.junit.jupiter.api.Assertions.assertNotNull;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.ArgumentMatchers.eq;
