985dc1568ad3db5dad0fe84dcc8eb8ed41ba28c7
Ivan <v23620@gmail.com>
2022-01-23 21:06:32 +0300

    make invalidation listener part of class

--- src/main/java/com/faforever/client/fx/contextmenu/ChatCategoryColorPickerCustomMenuItemController.java
+++ src/main/java/com/faforever/client/fx/contextmenu/ChatCategoryColorPickerCustomMenuItemController.java
@@ -6,6 +6,7 @@ import com.faforever.client.fx.JavaFxUtil;
 import com.faforever.client.preferences.ChatPrefs;
 import com.faforever.client.preferences.PreferencesService;
 import com.faforever.client.util.Assert;
+import javafx.beans.InvalidationListener;
 import javafx.beans.WeakInvalidationListener;
 import javafx.scene.control.Button;
 import javafx.scene.control.ColorPicker;
@@ -29,16 +30,17 @@ public class ChatCategoryColorPickerCustomMenuItemController extends AbstractCus
   public Button removeCustomColorButton;
 
   private ChatPrefs chatPrefs;
+  private final InvalidationListener chatColorModePropertyListener = (observable) -> JavaFxUtil.runLater(() -> {
+    ChatColorMode chatColorMode = chatPrefs.getChatColorMode();
+    removeCustomColorButton.setVisible(!chatColorMode.equals(RANDOM) && colorPicker.getValue() != null);
+    getRoot().setVisible(!chatColorMode.equals(RANDOM));
+  });
 
   public void initialize() {
     chatPrefs = preferencesService.getPreferences().getChat();
     removeCustomColorButton.setOnAction(event -> colorPicker.setValue(null));
     JavaFxUtil.bindManagedToVisible(removeCustomColorButton);
-    WeakInvalidationListener weakChatColorModePropertyListener = new WeakInvalidationListener((observable) -> JavaFxUtil.runLater(() -> {
-      ChatColorMode chatColorMode = chatPrefs.getChatColorMode();
-      removeCustomColorButton.setVisible(!chatColorMode.equals(RANDOM) && colorPicker.getValue() != null);
-      getRoot().setVisible(!chatColorMode.equals(RANDOM));
-    }));
+    WeakInvalidationListener weakChatColorModePropertyListener = new WeakInvalidationListener(chatColorModePropertyListener);
     JavaFxUtil.addListener(colorPicker.valueProperty(), weakChatColorModePropertyListener);
     JavaFxUtil.addAndTriggerListener(chatPrefs.chatColorModeProperty(), weakChatColorModePropertyListener);
   }
