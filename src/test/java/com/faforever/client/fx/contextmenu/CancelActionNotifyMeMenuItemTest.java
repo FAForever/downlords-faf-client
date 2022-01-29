40a7d673aa608dd7533b7fe328734931e58450a1
Ivan <v23620@gmail.com>
2022-01-30 02:26:38 +0300

    add CancelActionNotifyMeMenuItemTest.java

---
+++ src/test/java/com/faforever/client/fx/contextmenu/CancelActionNotifyMeMenuItemTest.java
@@ -0,0 +1,66 @@
+package com.faforever.client.fx.contextmenu;
+
+import com.faforever.client.builders.GameBeanBuilder;
+import com.faforever.client.domain.GameBean;
+import com.faforever.client.i18n.I18n;
+import com.faforever.client.replay.LiveReplayService;
+import com.faforever.client.replay.LiveReplayService.LiveReplayAction;
+import com.faforever.client.test.UITest;
+import javafx.util.Pair;
+import org.junit.jupiter.api.Test;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.Mockito;
+
+import java.util.Optional;
+
+import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.Mockito.when;
+
+public class CancelActionNotifyMeMenuItemTest extends UITest {
+
+  @Mock
+  private I18n i18n;
+  @Mock
+  private LiveReplayService liveReplayService;
+
+  @InjectMocks
+  private CancelActionNotifyMeMenuItem instance;
+
+  @Test
+  public void testOnClickedCancelActionNotifyMe() {
+    instance.onClicked();
+    Mockito.verify(liveReplayService).stopTrackingReplay();
+  }
+
+  @Test
+  public void testVisibleItem() {
+    GameBean game = GameBeanBuilder.create().defaultValues().get();
+    Mockito.when(liveReplayService.getTrackingReplay()).thenReturn(Optional.of(new Pair<>(game.getId(), LiveReplayAction.NOTIFY_ME)));
+
+    instance.setObject(game);
+    assertTrue(instance.isVisible());
+  }
+
+  @Test
+  public void testInvisibleItemIfNoGame() {
+    instance.setObject(null);
+    assertFalse(instance.isVisible());
+  }
+
+  @Test
+  public void testInvisibleItemIfNoStartTimeGame() {
+    GameBean game = GameBeanBuilder.create().defaultValues().startTime(null).get();
+    instance.setObject(game);
+    assertFalse(instance.isVisible());
+  }
+
+  @Test
+  public void testInvisibleItemIfNoTrackingOwnReplay() {
+    GameBean game = GameBeanBuilder.create().defaultValues().id(1).get();
+    when(liveReplayService.getTrackingReplay()).thenReturn(Optional.of(new Pair<>(2, LiveReplayAction.NOTIFY_ME)));
+
+    instance.setObject(game);
+    assertFalse(instance.isVisible());
+  }
+}
