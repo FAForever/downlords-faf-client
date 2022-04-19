a477e9d2b46cd70fcb48ca0b6d2ab1f5111045f8
Ivan <v23620@gmail.com>
2022-04-19 09:50:32 +0300

    fix tests by Sheikah45 (https://github.com/FAForever/downlords-faf-client/commit/01596b07c237d3fa09c0f823dd052091799466f2)

--- src/test/java/com/faforever/client/login/LoginControllerTest.java
+++ src/test/java/com/faforever/client/login/LoginControllerTest.java
@@ -81,6 +81,8 @@ public class LoginControllerTest extends UITest {
   @Mock
   private UiService uiService;
   @Mock
+  private PlatformService platformService;
+  @Mock
   private I18n i18n;
   @Mock
   private ClientUpdateService clientUpdateService;
