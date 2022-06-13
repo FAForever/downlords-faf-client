2daa09dc0c1bb4eedafd747de00f6531e0cfc16e
Sheikah45 <Sheikah450@gmail.com>
2022-06-13 12:33:16 -0400

    Remove runtime writing of steam_appid.txt

--- src/main/java/com/faforever/client/steam/SteamService.java
+++ src/main/java/com/faforever/client/steam/SteamService.java
@@ -7,30 +7,9 @@ import org.springframework.beans.factory.DisposableBean;
 import org.springframework.beans.factory.InitializingBean;
 import org.springframework.stereotype.Component;
 
-import java.io.FileOutputStream;
-import java.io.IOException;
-import java.io.InputStream;
-import java.nio.file.Files;
-import java.nio.file.Path;
-
 @Component
 @Slf4j
 public class SteamService implements InitializingBean, DisposableBean {
-  private static final String STEAM_ID_FILENAME = "steam_appid.txt";
-
-  public SteamService() {
-    Path steamAppIdPath = Path.of(STEAM_ID_FILENAME).toAbsolutePath();
-    if (!Files.exists(steamAppIdPath)) {
-      try (FileOutputStream outputStream = new FileOutputStream(steamAppIdPath.toFile()); InputStream inputStream = SteamService.class.getResourceAsStream("/" + STEAM_ID_FILENAME)) {
-        if (inputStream != null) {
-          byte[] steamIdBytes = inputStream.readAllBytes();
-          outputStream.write(steamIdBytes);
-        }
-      } catch (IOException e) {
-        log.warn("steam_appid.txt did not already exist and unable to write new one to installation directory. Steam integration will not work");
-      }
-    }
-  }
 
   public void afterPropertiesSet() {
     try {
