842d999bd8b0c9afc659782b98454e0f532caf69
vladislemon <vladislemon@gmail.com>
2022-09-07 07:11:01 +0300

    Change default Data Location to AppData/Roaming

--- src/main/java/com/faforever/client/preferences/DataPrefs.java
+++ src/main/java/com/faforever/client/preferences/DataPrefs.java
@@ -25,7 +25,7 @@ public class DataPrefs {
 
   static {
     if (org.bridj.Platform.isWindows()) {
-      DEFAULT_FAF_DATA_DIRECTORY = Path.of(Shell32Util.getFolderPath(ShlObj.CSIDL_COMMON_APPDATA), "FAForever");
+      DEFAULT_FAF_DATA_DIRECTORY = Path.of(Shell32Util.getFolderPath(ShlObj.CSIDL_APPDATA), "FAForever");
     } else {
       DEFAULT_FAF_DATA_DIRECTORY = Path.of(System.getProperty("user.home")).resolve(USER_HOME_SUB_FOLDER);
     }
