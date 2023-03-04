38d28ce76a3d8605b08ea77ca5704c205b727085
Sheikah45 <Sheikah450@gmail.com>
2023-03-04 15:34:31 -0500

    Don't dynamically create team and player card controllers

--- src/main/java/com/faforever/client/game/GameTooltipController.java
+++ src/main/java/com/faforever/client/game/GameTooltipController.java
@@ -4,18 +4,14 @@ package com.faforever.client.game;
 import com.faforever.client.domain.GameBean;
 import com.faforever.client.fx.Controller;
 import com.faforever.client.fx.JavaFxUtil;
-import com.faforever.client.fx.SimpleInvalidationListener;
 import com.faforever.client.theme.UiService;
 import com.faforever.client.util.RatingUtil;
 import com.google.common.base.Joiner;
 import javafx.beans.binding.Bindings;
-import javafx.beans.binding.IntegerBinding;
 import javafx.beans.property.BooleanProperty;
-import javafx.beans.property.ListProperty;
 import javafx.beans.property.MapProperty;
 import javafx.beans.property.ObjectProperty;
 import javafx.beans.property.SimpleBooleanProperty;
-import javafx.beans.property.SimpleListProperty;
 import javafx.beans.property.SimpleMapProperty;
 import javafx.beans.property.SimpleObjectProperty;
 import javafx.beans.value.ObservableValue;
@@ -46,7 +42,6 @@ public class GameTooltipController implements Controller<Node> {
   private final BooleanProperty showMods = new SimpleBooleanProperty(true);
   private final MapProperty<Integer, List<Integer>> teams = new SimpleMapProperty<>(FXCollections.emptyObservableMap());
   private final ObservableList<Integer> teamIds = new SortedList<>(JavaFxUtil.attachListToMapKeys(FXCollections.observableArrayList(), teams), Comparator.naturalOrder());
-  private final ListProperty<TeamCardController> teamCardControllers = new SimpleListProperty<>(FXCollections.observableArrayList());
   private final ObservableValue<String> leaderboard = game.flatMap(GameBean::leaderboardProperty);
 
 
@@ -64,9 +59,24 @@ public class GameTooltipController implements Controller<Node> {
             .flatMap(mods -> showMods.map(show -> show ? mods : "")));
 
     teams.bind(game.flatMap(GameBean::teamsProperty));
-    teams.addListener((SimpleInvalidationListener) this::onTeamsInvalidated);
 
     teamsPane.prefColumnsProperty().bind(Bindings.min(2, Bindings.size(teams)));
+
+    for (int i = -1; i < 8; i++) {
+      TeamCardController teamCardController = uiService.loadFxml("theme/team_card.fxml");
+      teamCardController.bindPlayersToPlayerIds();
+      teamCardController.setRatingPrecision(RatingPrecision.ROUNDED);
+      teamCardController.ratingProviderProperty()
+          .bind(leaderboard.map(name -> player -> RatingUtil.getLeaderboardRating(player, name)));
+      teamCardController.playerIdsProperty()
+          .bind(Bindings.valueAt(teams, teamCardController.teamIdProperty().asObject())
+              .map(FXCollections::observableList));
+      teamCardController.teamIdProperty().bind(Bindings.valueAt(teamIds, i + 1));
+      Node teamCardControllerRoot = teamCardController.getRoot();
+      teamCardControllerRoot.visibleProperty().bind(teamCardController.playerIdsProperty().emptyProperty().not());
+      JavaFxUtil.bindManagedToVisible(teamCardControllerRoot);
+      teamsPane.getChildren().add(teamCardControllerRoot);
+    }
   }
 
   public void setGame(GameBean game) {
@@ -79,32 +89,6 @@ public class GameTooltipController implements Controller<Node> {
 
   public ObjectProperty<GameBean> gameProperty() {
     return game;
-  }
-
-  private void onTeamsInvalidated() {
-    int numTeams = teams.size();
-    int numControllers = teamCardControllers.size();
-    int difference = numTeams - numControllers;
-    if (difference > 0) {
-      for (int i = 0; i < difference; i++) {
-        TeamCardController teamCardController = uiService.loadFxml("theme/team_card.fxml");
-        teamCardController.bindPlayersToPlayerIds();
-        teamCardController.setRatingPrecision(RatingPrecision.ROUNDED);
-        teamCardController.ratingProviderProperty()
-            .bind(leaderboard.map(name -> player -> RatingUtil.getLeaderboardRating(player, name)));
-        teamCardController.playerIdsProperty()
-            .bind(Bindings.valueAt(teams, teamCardController.teamIdProperty().asObject())
-                .map(FXCollections::observableList));
-        IntegerBinding indexBinding = Bindings.createIntegerBinding(() -> teamCardControllers.indexOf(teamCardController), teamCardControllers);
-        teamCardController.teamIdProperty().bind(Bindings.valueAt(teamIds, indexBinding));
-        teamCardControllers.add(teamCardController);
-        teamsPane.getChildren().add(teamCardController.getRoot());
-      }
-    } else if (difference < 0) {
-      int from = numControllers + difference;
-      teamCardControllers.remove(from, numControllers);
-      teamsPane.getChildren().remove(from, numControllers);
-    }
   }
 
   public void setShowMods(boolean showMods) {
