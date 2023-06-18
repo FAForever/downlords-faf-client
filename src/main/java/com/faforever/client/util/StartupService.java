6b063ea845589684775a2f47b09d351df9597f31
Sheikah45 <66929319+Sheikah45@users.noreply.github.com>
2023-06-17 18:29:27 -0700

    Warn when one drive in vault path (#2998)

--- src/main/java/com/faforever/client/util/StartupService.java
+++ src/main/java/com/faforever/client/util/StartupService.java
@@ -1,8 +1,9 @@
-package com.faforever.client;
+package com.faforever.client.util;
 
 import com.faforever.client.fx.FxApplicationThreadExecutor;
 import com.faforever.client.i18n.I18n;
 import com.faforever.client.os.OperatingSystem;
+import com.faforever.client.preferences.ForgedAlliancePrefs;
 import com.faforever.client.preferences.GeneralPrefs;
 import javafx.scene.control.Alert;
 import javafx.scene.control.Alert.AlertType;
@@ -22,6 +23,7 @@ public class StartupService {
   private final OperatingSystem operatingSystem;
   private final I18n i18n;
   private final GeneralPrefs generalPrefs;
+  private final ForgedAlliancePrefs forgedAlliancePrefs;
   private final FxApplicationThreadExecutor fxApplicationThreadExecutor;
 
   private void checkCyrillic() {
@@ -62,9 +64,21 @@ public class StartupService {
     });
   }
 
+  private void checkOneDrive() {
+    if (!forgedAlliancePrefs.getVaultBaseDirectory().toString().contains("OneDrive")) {
+      return;
+    }
+
+    fxApplicationThreadExecutor.executeAndWait(() -> {
+      Alert alert = new Alert(AlertType.WARNING, "Maps and Mods path detected to be in One Drive. One Drive can cause issues during the game as it has a lock on the files  during synchronization. It is advised that you change your maps and mods path in the client settings", ButtonType.OK);
+      alert.showAndWait();
+    });
+  }
+
   @EventListener(value = ApplicationReadyEvent.class)
   public void checkSystem() {
     checkAdmin();
     checkCyrillic();
+    checkOneDrive();
   }
 }
