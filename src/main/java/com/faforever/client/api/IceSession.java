2a8d798c4801e9191e0f283890f8bd7957798f45
Sheikah45 <Sheikah450@gmail.com>
2023-10-31 18:54:44 -0400

    Remove ignore unknown properties

--- src/main/java/com/faforever/client/api/IceSession.java
+++ src/main/java/com/faforever/client/api/IceSession.java
@@ -1,11 +1,9 @@
 package com.faforever.client.api;
 
 import com.faforever.commons.api.dto.CoturnServer;
-import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import org.jetbrains.annotations.NotNull;
 
 import java.util.List;
 
-@JsonIgnoreProperties(ignoreUnknown = true)
 public record IceSession(@NotNull String id, @NotNull List<CoturnServer> servers) {
 }
