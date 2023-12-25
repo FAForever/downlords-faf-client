6ac384eb45d4b9a46e30bd88670067074822af1a
Sheikah45 <Sheikah450@gmail.com>
2023-12-24 20:07:05 -0500

    Simplify color picker logic

--- src/main/java/com/faforever/client/fx/contextmenu/ChatUserColorPickerCustomMenuItemController.java
+++ src/main/java/com/faforever/client/fx/contextmenu/ChatUserColorPickerCustomMenuItemController.java
@@ -1,7 +1,6 @@
 package com.faforever.client.fx.contextmenu;
 
 import com.faforever.client.chat.ChatChannelUser;
-import com.faforever.client.chat.ChatUserCategory;
 import com.faforever.client.fx.JavaFxUtil;
 import com.faforever.client.preferences.ChatPrefs;
 import com.faforever.client.util.Assert;
@@ -46,18 +45,10 @@ public class ChatUserColorPickerCustomMenuItemController extends AbstractColorPi
   }
 
   private void updateUserColor() {
-    ChatUserCategory userCategory;
-    if (object.isAway()) {
-      userCategory = ChatUserCategory.AWAY;
-    } else if (object.isModerator()) {
-      userCategory = ChatUserCategory.MODERATOR;
-    } else {
-      userCategory = object.getCategory();
-    }
     Color newColor = colorPicker.getValue();
     if (newColor == null) {
       chatPrefs.getUserToColor().remove(getLowerUsername(object));
-      object.setColor(chatPrefs.getGroupToColor().getOrDefault(userCategory, null));
+      object.setColor(chatPrefs.getGroupToColor().getOrDefault(object.getCategory(), null));
     } else {
       chatPrefs.getUserToColor().put(getLowerUsername(object), newColor);
       object.setColor(newColor);
