ad736a0cd423cb4febd4155c66d1939edf3da378
Patrick McCusker <patmccusker14@gmail.com>
2025-06-07 18:20:56 -0400

    Revert "Use UriComponentsBuilder"
    This reverts commit 4b25ddbfee5f7ebef700adbb16af0be5e6c8f3bd.

--- src/main/java/com/faforever/client/user/LoginService.java
+++ src/main/java/com/faforever/client/user/LoginService.java
@@ -20,7 +20,6 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.InitializingBean;
 import org.springframework.context.annotation.Lazy;
 import org.springframework.stereotype.Service;
-import org.springframework.web.util.UriComponentsBuilder;
 import reactor.core.publisher.Mono;
 import reactor.function.TupleUtils;
 
@@ -65,16 +64,8 @@ public class LoginService implements InitializingBean {
     String codeChallenge = BASE64_ENCODER.encodeToString(Hashing.sha256()
         .hashString(codeVerifier, StandardCharsets.US_ASCII)
         .asBytes());
-
-    return UriComponentsBuilder.fromUriString(oauth.getBaseUrl())
-        .path("/oauth2/auth")
-        .queryParam("response_type", "code")
-        .queryParam("client_id", oauth.getClientId())
-        .queryParam("state", state)
-        .queryParam("redirect_uri", redirectUri.toASCIIString())
-        .queryParam("scope", scopes)
-        .queryParam("code_challenge_method", codeChallenge)
-        .build().toUriString();
+    return String.format("%s/oauth2/auth?response_type=code&client_id=%s&state=%s&redirect_uri=%s&scope=%s&code_challenge_method=S256&code_challenge=%s", oauth.getBaseUrl(), oauth.getClientId(), state, redirectUri.toASCIIString(),
+                         scopes, codeChallenge);
   }
 
   public Mono<Void> login(String code, String codeVerifier, URI redirectUri) {
