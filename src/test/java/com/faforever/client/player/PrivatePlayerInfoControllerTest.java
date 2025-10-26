1cf668a01a91a3116379306d3898825452865d34
MarcSpector <v23620git@gmail.com>
2025-10-26 20:55:27 +0300

    fix PrivatePlayerInfoControllerTest class

--- src/test/java/com/faforever/client/player/PrivatePlayerInfoControllerTest.java
+++ src/test/java/com/faforever/client/player/PrivatePlayerInfoControllerTest.java
@@ -1,12 +1,9 @@
 package com.faforever.client.player;
 
 import com.faforever.client.achievements.AchievementService;
-import com.faforever.client.builders.ChatChannelUserBuilder;
 import com.faforever.client.builders.GameInfoBuilder;
 import com.faforever.client.builders.LeaderboardRatingMapBuilder;
 import com.faforever.client.builders.PlayerInfoBuilder;
-import com.faforever.client.chat.ChatChannel;
-import com.faforever.client.chat.ChatChannelUser;
 import com.faforever.client.domain.api.Leaderboard;
 import com.faforever.client.domain.server.PlayerInfo;
 import com.faforever.client.game.GameDetailController;
@@ -14,7 +11,8 @@ import com.faforever.client.i18n.I18n;
 import com.faforever.client.leaderboard.LeaderboardService;
 import com.faforever.client.replay.WatchButtonController;
 import com.faforever.client.test.PlatformTest;
-import com.faforever.client.theme.UiService;
+import com.faforever.commons.lobby.GameStatus;
+import javafx.beans.property.ObjectProperty;
 import javafx.beans.property.SimpleObjectProperty;
 import org.instancio.Instancio;
 import org.junit.jupiter.api.BeforeEach;
@@ -38,10 +36,7 @@ import static org.mockito.Mockito.lenient;
 import static org.mockito.Mockito.verify;
 
 public class PrivatePlayerInfoControllerTest extends PlatformTest {
-  private static final String USERNAME = "junit";
 
-  @Mock
-  private UiService uiService;
   @Mock
   private I18n i18n;
   @Mock
@@ -53,24 +48,30 @@ public class PrivatePlayerInfoControllerTest extends PlatformTest {
   @Mock
   private WatchButtonController watchButtonController;
 
-
   @InjectMocks
   private PrivatePlayerInfoController instance;
-  private PlayerInfo player;
-  private ChatChannelUser chatChannelUser;
+
+  private PlayerInfo playerInfo;
+  ObjectProperty<PlayerInfo> player;
   private Leaderboard leaderboard;
 
   @BeforeEach
   public void setUp() throws Exception {
     leaderboard = Instancio.of(Leaderboard.class).set(field(Leaderboard::technicalName), "global").create();
-    player = PlayerInfoBuilder.create().defaultValues().game(null).get();
-    chatChannelUser = ChatChannelUserBuilder.create(USERNAME, new ChatChannel("testChannel"))
-                                            .defaultValues()
-                                            .player(player)
-                                            .get();
+    playerInfo = PlayerInfoBuilder.create()
+                                  .defaultValues()
+                                  .game(null)
+                                  .leaderboardRatings(LeaderboardRatingMapBuilder.create()
+                                                                                 .put(leaderboard.technicalName(),
+                                                                                      Instancio.create(
+                                                                                          LeaderboardRating.class))
+                                                                                 .get())
+                                  .get();
+    player = new SimpleObjectProperty<>(playerInfo);
+    instance.playerProperty().bind(player);
 
     lenient().when(gameDetailController.gameProperty()).thenReturn(new SimpleObjectProperty<>());
-    lenient().when(achievementService.getPlayerAchievements(player.getId())).thenReturn(Flux.empty());
+    lenient().when(achievementService.getPlayerAchievements(playerInfo.getId())).thenReturn(Flux.empty());
     lenient().when(achievementService.getAchievementDefinitions()).thenReturn(Flux.empty());
     lenient().when(leaderboardService.getLeaderboards()).thenReturn(Flux.just(leaderboard));
     lenient().when(i18n.getOrDefault(leaderboard.technicalName(), leaderboard.nameKey()))
@@ -91,15 +92,7 @@ public class PrivatePlayerInfoControllerTest extends PlatformTest {
   }
 
   @Test
-  public void testSetChatUserWithPlayerNoGame() {
-    player.setLeaderboardRatings(LeaderboardRatingMapBuilder.create()
-                                                            .put(leaderboard.technicalName(),
-                                                                 Instancio.create(LeaderboardRating.class))
-                                                            .get());
-
-    instance.setChatUser(chatChannelUser);
-    WaitForAsyncUtils.waitForFxEvents();
-
+  public void testCheckGameInfoForIdlePlayer() {
     assertTrue(instance.userImageView.isVisible());
     assertTrue(instance.country.isVisible());
     assertTrue(instance.ratingsLabels.isVisible());
@@ -114,59 +107,47 @@ public class PrivatePlayerInfoControllerTest extends PlatformTest {
     assertTrue(instance.ratingsValues.getText().contains("123"));
     assertEquals("0/0", instance.unlockedAchievements.getText());
     assertEquals("123", instance.gamesPlayed.getText());
-    verify(achievementService).getPlayerAchievements(player.getId());
   }
 
   @Test
-  public void testSetChatUserWithPlayerWithGame() {
-    player.setGame(GameInfoBuilder.create().defaultValues().get());
-    instance.setChatUser(chatChannelUser);
-    WaitForAsyncUtils.waitForFxEvents();
+  public void testCheckGameInfoForPlayerWithGame() {
+    runOnFxThreadAndWait(() -> playerInfo.setGame(GameInfoBuilder.create().status(GameStatus.PLAYING).get()));
 
     assertTrue(instance.gameDetailWrapper.isVisible());
   }
 
   @Test
-  public void testSetChatUserWithPlayerNoGameThenJoinsGame() {
-    instance.setChatUser(chatChannelUser);
-    WaitForAsyncUtils.waitForFxEvents();
-
+  public void testCheckGameInfoPlayerNoGameThenJoinsGame() {
     assertFalse(instance.gameDetailWrapper.isVisible());
 
-    player.setGame(GameInfoBuilder.create().defaultValues().get());
+    runOnFxThreadAndWait(() -> playerInfo.setGame(GameInfoBuilder.create().defaultValues().get()));
 
     assertTrue(instance.gameDetailWrapper.isVisible());
   }
 
   @Test
   public void testSetChatUserLeavesGame() {
-    instance.setChatUser(chatChannelUser);
-    player.setGame(GameInfoBuilder.create().defaultValues().get());
+    runOnFxThreadAndWait(() -> instance.playerProperty().setValue(playerInfo));
+    playerInfo.setGame(GameInfoBuilder.create().defaultValues().get());
     WaitForAsyncUtils.waitForFxEvents();
 
     assertTrue(instance.gameDetailWrapper.isVisible());
 
-    player.setGame(null);
+    playerInfo.setGame(null);
 
     assertFalse(instance.gameDetailWrapper.isVisible());
   }
 
   @Test
   public void testRatingChange() {
-    instance.setChatUser(chatChannelUser);
-    WaitForAsyncUtils.waitForFxEvents();
-
-    player.setLeaderboardRatings(Map.of("test", Instancio.create(LeaderboardRating.class)));
-    WaitForAsyncUtils.waitForFxEvents();
+    runOnFxThreadAndWait(() -> playerInfo.setLeaderboardRatings(Map.of("test", Instancio.create(LeaderboardRating.class))));
 
     verify(leaderboardService).getLeaderboards();
   }
 
   @Test
   public void testSetChatUserWithNoPlayer() {
-    chatChannelUser.setPlayer(null);
-    instance.setChatUser(chatChannelUser);
-    WaitForAsyncUtils.waitForFxEvents();
+    runOnFxThreadAndWait(() -> player.setValue(null));
 
     assertFalse(instance.userImageView.isVisible());
     assertFalse(instance.country.isVisible());
@@ -180,9 +161,7 @@ public class PrivatePlayerInfoControllerTest extends PlatformTest {
 
   @Test
   public void testSetChatUserWithNoPlayerThenGetsPlayer() {
-    chatChannelUser.setPlayer(null);
-    instance.setChatUser(chatChannelUser);
-    WaitForAsyncUtils.waitForFxEvents();
+    runOnFxThreadAndWait(() -> player.setValue(null));
 
     assertFalse(instance.userImageView.isVisible());
     assertFalse(instance.country.isVisible());
@@ -193,12 +172,11 @@ public class PrivatePlayerInfoControllerTest extends PlatformTest {
     assertFalse(instance.unlockedAchievements.isVisible());
     assertFalse(instance.unlockedAchievementsLabel.isVisible());
 
-    player.setLeaderboardRatings(LeaderboardRatingMapBuilder.create()
-                                                            .put(leaderboard.technicalName(),
+    playerInfo.setLeaderboardRatings(LeaderboardRatingMapBuilder.create()
+                                                                .put(leaderboard.technicalName(),
                                                                  Instancio.create(LeaderboardRating.class))
-                                                            .get());
-    chatChannelUser.setPlayer(player);
-    WaitForAsyncUtils.waitForFxEvents();
+                                                                .get());
+    runOnFxThreadAndWait(() -> player.setValue(playerInfo));
 
     assertTrue(instance.userImageView.isVisible());
     assertTrue(instance.country.isVisible());
@@ -214,14 +192,16 @@ public class PrivatePlayerInfoControllerTest extends PlatformTest {
     assertTrue(instance.ratingsValues.getText().contains("123"));
     assertEquals("0/0", instance.unlockedAchievements.getText());
     assertEquals("123", instance.gamesPlayed.getText());
-    verify(achievementService).getPlayerAchievements(player.getId());
+  }
+
+  @Test
+  public void testLoadAchievementsIfPlayerIsNotNull() {
+    assertNotNull(instance.playerProperty().getValue());
+    verify(achievementService).getPlayerAchievements(playerInfo.getId());
   }
 
   @Test
   public void testGetRoot() {
-    instance.setChatUser(chatChannelUser);
-    WaitForAsyncUtils.waitForFxEvents();
-
     assertEquals(instance.privateUserInfoRoot, instance.getRoot());
   }
 }
